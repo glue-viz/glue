f5cbfd20d746ed158d46e4467aac255df37ee244
Thomas Robitaille <thomas.robitaille@gmail.com>
2011-05-23 15:34:43 -0400

    Updated simple test

--- examples/simple_comm.py
+++ examples/simple_comm.py
@@ -17,8 +17,8 @@ class SimpleClient(Client):
     the data it is associated with
     """
 
-    def __init__(self, hub, data, name="SimpleClient"):
-        Client.__init__(self, hub, data)
+    def __init__(self, data, name="SimpleClient"):
+        Client.__init__(self, data)
         self.name = name
 
     def __str__(self):
@@ -32,13 +32,13 @@ class SimpleClient(Client):
                              delete=delete)
 
     def _add_subset(self, subset):
-        print "Client is %s\n\t Added a subset: %s" % (self, subset)
+        print "Client is %s\n    Added a subset: %s" % (self, subset)
 
     def _delete_subset(self, subset):
-        print "Client is %s\n\t Deleted subset: %s" % (self, subset)
+        print "Client is %s\n    Deleted subset: %s" % (self, subset)
 
     def _refresh_subset(self, subset, attr):
-        print ("Client is %s\n\t Modified a subset: %s\n\t "
+        print ("Client is %s\n    Modified a subset: %s\n    "
                "Changed attribute %s" % (self, subset, attr))
 
 
@@ -46,9 +46,10 @@ class SimpleSubset(Subset):
     """A simple subset with a name."""
 
     def __init__(self, data, name="SimpleSubset"):
-       self.name = name
-       # call superclass after setting self.name, to prevent the hub from relaying the message
-       Subset.__init__(self, data)
+        self.name = name
+        # call superclass after setting self.name, to prevent the hub from
+        # relaying the message
+        Subset.__init__(self, data)
 
     def __str__(self):
         return self.name
@@ -59,62 +60,59 @@ def run_tests():
 
     >>> run_tests()
         Client is C1
-        Modified a subset: S1
-        Changed attribute modification1
+            Modified a subset: S1
+            Changed attribute modification1
         Client is C1
-        Added a subset: S2
+            Added a subset: S2
         Client is C2
-        Added a subset: S2
+            Added a subset: S2
         Client is C1
-        Modified a subset: S2
-        Changed attribute modification2
+            Modified a subset: S2
+            Changed attribute modification2
         Client is C2
-        Modified a subset: S2
-        Changed attribute modification2
+            Modified a subset: S2
+            Changed attribute modification2
         Client is C3
-        Added a subset: S3
+            Added a subset: S3
         Client is C1
-        Modified a subset: S1
-        Changed attribute modification3
+            Modified a subset: S1
+            Changed attribute modification3
         Client is C2
-        Modified a subset: S1
-        Changed attribute modification3
+            Modified a subset: S1
+            Changed attribute modification3
         Client is C1
-        Modified a subset: S1
-        Changed attribute modification4
+            Modified a subset: S1
+            Changed attribute modification4
     """
 
     d = Data()
     h = Hub()
-    c1 = SimpleClient(h, d, name="C1")
-    c2 = SimpleClient(h, d, name='C2')
+    c1 = SimpleClient(d, name="C1")
+    c2 = SimpleClient(d, name='C2')
     s1 = SimpleSubset(d)
     s1.name = 'S1'
 
-    #test modification with single client
+    # Test modification with single client
     h.add_client(c1)
-    s1.modification1=1
+    s1.modification1 = 1
 
-    # test creation and modification with 2 clients
-
+    # Test creation and modification with 2 clients
     h.add_client(c2)
-    s2 = SimpleSubset(d, name = "S2")
+    s2 = SimpleSubset(d, name="S2")
+    s2.modification2 = 1
 
-    s2.modification2=1
-
-    # test independent data + clients
+    # Test independent data + clients
     d2 = Data()
-    c3 = SimpleClient(h, d2, name="C3")
+    c3 = SimpleClient(d2, name="C3")
     h.add_client(c3)
 
     s3 = SimpleSubset(d2, name="S3")
     s1.modification3 = 1
 
-    # test removal
+    # Test client removal
     h.remove_client(c2)
     s1.modification4 = 1
 
 if __name__ == "__main__":
     import doctest
     doctest.testmod(optionflags=doctest.NORMALIZE_WHITESPACE)
-
