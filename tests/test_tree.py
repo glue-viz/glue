5ae48b6199a28449c38d1ddf0762f2a2a3e51420
Chris Beaumont <beaumont@hawaii.edu>
2011-06-02 13:19:17 -0400

    added tree_traversal classes

--- tests/test_tree.py
+++ tests/test_tree.py
@@ -2,6 +2,7 @@ import unittest
 import numpy as np
 from cloudviz.tree import Tree, NewickTree, DendroMerge
 
+
 class TestTree(unittest.TestCase):
 
     def test_tree_creation(self):
@@ -39,18 +40,21 @@ class TestTree(unittest.TestCase):
         self.assertIn(c211, c21.children)
 
     def test_newick_tree(self):
-        
+
         # no labels
+        n0 = "0;"
         n1 = "(0,1)2;"
         n2 = "((0,1)4,(2,3)5)6;"
         n3 = "(3,(2,(0,1)4)5)6;"
         n4 = "(1,2,3,4)5;"
-        
+
+        tree0 = NewickTree(n0)
         tree1 = NewickTree(n1)
         tree2 = NewickTree(n2)
         tree3 = NewickTree(n3)
         tree4 = NewickTree(n4)
-        
+
+        self.assertEquals(tree0.id, 0)
         self.assertEquals(tree1.id, 2)
         self.assertFalse(tree1.value)
         self.assertIn(0, [x.id for x in tree1.children])
@@ -62,7 +66,6 @@ class TestTree(unittest.TestCase):
         self.assertIn(5, [x.id for x in tree2.children])
         self.assertNotIn(0, [x.id for x in tree2.children])
         self.assertNotIn(1, [x.id for x in tree2.children])
-        
 
         self.assertIn(1, [x.id for x in tree4.children])
         self.assertIn(2, [x.id for x in tree4.children])
@@ -78,30 +81,29 @@ class TestTree(unittest.TestCase):
         n1 = "(0:0,1:10)2:20;"
         n2 = "((0:0,1:10)4:4,(2:20,3:30)5:50)6:60;"
         n3 = "(3:30,(2:20,(0:0,1:10)4:40)5:50)6:60;"
-        
+
         tree1 = NewickTree(n1)
         tree2 = NewickTree(n2)
         tree3 = NewickTree(n3)
-        
+
         self.assertEquals(tree1.value, '20')
-        
+
         self.assertEqual(n1, tree1.to_newick())
         self.assertEqual(n2, tree2.to_newick())
         self.assertEqual(n3, tree3.to_newick())
 
     def test_dendro_merge(self):
-        m1 = np.array( [ [0,1], [2,3], [4,5] ] )
+        m1 = np.array([[0, 1], [2, 3], [4, 5]])
         n1 = "((0,1)4,(2,3)5)6;"
-        m2 = np.array( [ [0,1], [4,2], [5,3] ])
+        m2 = np.array([[0, 1], [4, 2], [5, 3]])
         n2 = "(3,(2,(0,1)4)5)6;"
         t1 = DendroMerge(m1)
         t2 = DendroMerge(m2)
 
         #invalid merge lists
-        m3 = np.array( [ [0,1], [5,2], [5,3] ] )
-        m4 = np.array( [ [-1,1], [4,2], [5,3] ] )
-        m5 = np.array( [ [0,1], [1,2], [5,3] ] )
-
+        m3 = np.array([[0, 1], [5, 2], [5, 3]])
+        m4 = np.array([[-1, 1], [4, 2], [5, 3]])
+        m5 = np.array([[0, 1], [1, 2], [5, 3]])
 
         self.assertEqual(t1.to_newick(), n1)
         self.assertEqual(t2.to_newick(), n2)
