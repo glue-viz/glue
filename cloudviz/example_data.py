277dfaec5082e25020136d04797ce8c070aed1f8
Chris Beaumont <beaumont@hawaii.edu>
2012-04-10 12:02:17 -0400

    tweaked loading style of example data

--- cloudviz/example_data.py
+++ cloudviz/example_data.py
@@ -3,9 +3,10 @@ import cloudviz as cv
 def pipe():
     data = cv.data.TabularData(label="Pipe YSOs")
     data2 = cv.data.TabularData(label="Pipe Cores")
-    data.read_data('../examples/pipe_yso.txt', type='ascii', delimiter='\t', data_start = 2)
+    data.read_data('../examples/pipe_yso.txt', type='ascii',
+                   delimiter='\t', data_start = 2)
     s = cv.subset.RoiSubset(data, label="YSO subset")
-    data2.read_data('../examples/pipe_cores.vot')
+    data2.read_data('../examples/pipe_cores.vot', type='vo')
     s2 = cv.subset.RoiSubset(data2, label="Core Subset")
 
     return data, data2, s, s2
@@ -24,7 +25,7 @@ def simple_image():
 
 
 def simple_cube():
-    data = cv.io.extract_data_fits('../examples/cube.fits')
+    data = cv.io.extract_data_fits('../examples/cps_12co_05.fits')
     comp = cv.data.Component(data['PRIMARY'])
     comp2 = cv.data.Component(data['PRIMARY'] * -1)
     data = cv.Data(label="Dummy Cube")
