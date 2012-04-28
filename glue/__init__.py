6872aaff6b4311e19d6ea0b4c3c50ce6ed7cd983
Chris Beaumont <beaumont@hawaii.edu>
2012-04-28 10:23:21 -1000

    cleaned up __init__

--- glue/__init__.py
+++ glue/__init__.py
@@ -4,14 +4,8 @@ from data import Component, Data, TabularData, GriddedData
 from tree import Tree, NewickTree
 from subset import Subset
 from message import Message
-from roi import MplCircularROI
 from scatter_client import ScatterClient
 from data_dendro_cpp import data_dendro_cpp
-from mpl_tree_client import MplTreeClient
-from tree_layout import TreeLayout
-from mpl_scatter_helper import RasterAxes
-from mpl_histogram_client import MplHistogramClient
 import example_data
 from data_collection import DataCollection
-from viz_client import VizClient
 import qt
