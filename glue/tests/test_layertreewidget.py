a4c3581c0869c2055eaedb3585feb0f759641570
Chris Beaumont <beaumont@hawaii.edu>
2012-04-28 10:37:14 -1000

    extensions to layer tree tests

--- glue/tests/test_layertreewidget.py
+++ glue/tests/test_layertreewidget.py
@@ -4,23 +4,15 @@ from threading import Thread
 from PyQt4.QtGui import QApplication, QMainWindow
 from PyQt4.QtTest import QTest
 from PyQt4.QtCore import Qt
+from PyQt4.QtGui import QItemSelectionModel
+
+from mock import MagicMock, patch
 
 import glue
 from glue.qt.layertreewidget import LayerTreeWidget
+from glue.subset import OrState, AndState, XorState
 
 from glue.qt.qtutil import DebugClipboard
-
-
-class SignalCatcher(object):
-    def __init__(self):
-        self.args = []
-        self.kwargs = {}
-        self.catches = 0
-
-    def catch(self, *args, **kwargs):
-        self.args = args
-        self.kwargs = kwargs
-        self.catches += 1
 
 class TestLayerTree(unittest.TestCase):
     """ Unit tests for the layertreewidget class """
@@ -34,11 +26,11 @@ class TestLayerTree(unittest.TestCase):
         self.widget = LayerTreeWidget()
         self.win = QMainWindow()
         self.win.setCentralWidget(self.widget)
-        self.win.show()
 
     def tearDown(self):
         self.win.close()
-        self.app.exit()
+        del self.win
+        del self.app
 
     def add_layer_via_method(self):
         """ Add first layer by invoking method"""
@@ -68,24 +60,21 @@ class TestLayerTree(unittest.TestCase):
 
     def layer_present(self, layer):
         """ Test that a layer exists in the widget """
-        return self.widget.is_layer_present(layer)
+        return self.widget.is_layer_present(layer) and \
+            layer.data in self.widget.data_collection
 
-    def layer_position(self, layer):
-        widget_item = self.widget[layer]
-        rect = self.widget.layerTree.visualItemRect(widget_item)
-        return rect.center()
-
-    def test_current_layer(self):
+    def test_current_layer_method_correct(self):
         layer = self.add_layer_via_method()
         item = self.widget[layer]
         self.widget.layerTree.setCurrentItem(item)
         self.assertIs(self.widget.current_layer(), layer)
 
-    def test_current_layer_null(self):
+    def test_current_layer_null_on_creation(self):
         self.assertIs(None, self.widget.current_layer())
 
-    def test_add_layer(self):
+    def test_add_layer_method(self):
         """ Test that a layer exists once added """
+        self.assertFalse(self.layer_present(self.data[0]))
         layer = self.add_layer_via_method()
         self.assertTrue(self.layer_present(layer))
 
@@ -94,6 +83,7 @@ class TestLayerTree(unittest.TestCase):
         layer = self.add_layer_via_method()
         self.remove_layer_via_button(layer)
         self.assertFalse(self.layer_present(layer))
+        self.assertFalse(layer in self.widget.data_collection)
 
     def test_empty_removal_does_nothing(self):
         """ Make sure widgets are only removed when selected """
@@ -123,7 +113,7 @@ class TestLayerTree(unittest.TestCase):
         widget_item = self.widget[layer]
         self.assertTrue(str(widget_item.text(3)) == "999")
 
-    def test_orphan_subset_grabs_parent(self):
+    def test_data_added_with_subset_add(self):
         """ Test that subset layers are added properly """
         layer = self.data[0]
         sub = glue.Subset(layer)
@@ -131,24 +121,216 @@ class TestLayerTree(unittest.TestCase):
         self.assertTrue(self.layer_present(sub))
         self.assertTrue(self.layer_present(layer))
 
-    @unittest.expectedFailure
-    def test_drag_functional(self):
-        """still not implemented"""
-        clipboard = DebugClipboard()
-        clipboard.show()
+    def test_subsets_added_with_data(self):
         layer = self.add_layer_via_method()
-        self.drag_layer_to_clipboard(layer, clipboard)
-        self.assertTrue(clipboard.get_text() == layer.label)
+        self.assertTrue(self.layer_present(layer.subsets[0]))
 
-    def test_signals(self):
+    def test_check_signal(self):
         layer = self.add_layer_via_method()
-        sc = SignalCatcher()
-        self.widget.layer_check_changed.connect(sc.catch)
+        sc = MagicMock()
+        self.widget._layer_check_changed.connect(sc.catch)
         item = self.widget[layer]
         item.setCheckState(0, Qt.Unchecked)
-        self.assertEquals(sc.catches, 1)
-        self.assertEquals(sc.args[0], layer)
-        self.assertEquals(sc.args[1], False)
+        sc.catch.assert_called_once_with(layer, False)
+
+    def test_new_subset_action(self):
+        layer = self.add_layer_via_method()
+        item = self.widget[layer]
+        self.widget.layerTree.setCurrentItem(item)
+        self.widget._new_action.trigger()
+        self.assertEquals(len(layer.subsets), 2)
+
+    def test_duplicate_subset_action(self):
+        layer = self.add_layer_via_method()
+        item = self.widget[layer.subsets[0]]
+        self.widget.layerTree.setCurrentItem(item)
+        self.widget._duplicate_action.trigger()
+        self.assertEquals(len(layer.subsets), 2)
+
+    def test_copy_paste_subset_action(self):
+        layer = self.add_layer_via_method()
+        item = self.widget[layer.subsets[0]]
+        self.widget.layerTree.setCurrentItem(item)
+        self.widget._copy_action.trigger()
+        self.assertTrue(self.widget._clipboard is not None)
+        sub = layer.new_subset()
+        self.widget.add_layer(sub)
+        item = self.widget[sub]
+        self.widget.layerTree.setCurrentItem(item)
+        state0 = sub.subset_state
+        self.widget._paste_action.trigger()
+        self.assertTrue(sub.subset_state is not state0)
+
+    def setup_two_subset_selection(self):
+        layer = self.add_layer_via_method()
+        s2 = layer.new_subset()
+        self.widget.add_layer(s2)
+        item1 = self.widget[layer.edit_subset]
+        item2 = self.widget[s2]
+        self.widget.layerTree.setCurrentItem(item1, 0,
+                                             QItemSelectionModel.Toggle)
+        self.widget.layerTree.setCurrentItem(item2, 0,
+                                             QItemSelectionModel.Toggle)
+        self.assertEquals(len(self.widget.layerTree.selectedItems()), 2)
+        return layer
+
+    def test_or_combine(self):
+        layer = self.setup_two_subset_selection()
+        old_subsets = set(layer.subsets)
+        self.widget._or_action.trigger()
+        new_subsets = set(layer.subsets)
+        diff = list(old_subsets ^ new_subsets)
+        self.assertTrue(len(diff) == 1)
+        self.assertTrue(isinstance(diff[0].subset_state, OrState))
+
+    def test_and_combine(self):
+        layer = self.setup_two_subset_selection()
+        old_subsets = set(layer.subsets)
+        self.widget._and_action.trigger()
+        new_subsets = set(layer.subsets)
+        diff = list(old_subsets ^ new_subsets)
+        self.assertTrue(len(diff) == 1)
+        self.assertTrue(isinstance(diff[0].subset_state, AndState))
+
+    def test_xor_combine(self):
+        layer = self.setup_two_subset_selection()
+        old_subsets = set(layer.subsets)
+        self.widget._xor_action.trigger()
+        new_subsets = set(layer.subsets)
+        diff = list(old_subsets ^ new_subsets)
+        self.assertTrue(len(diff) == 1)
+        self.assertTrue(isinstance(diff[0].subset_state, XorState))
+
+
+    def test_actions_enabled_single_subset_selection(self):
+        layer = self.add_layer_via_method()
+        item = self.widget[layer.edit_subset]
+        self.widget.layerTree.setCurrentItem(item)
+
+        self.assertFalse(self.widget._or_action.isEnabled())
+        self.assertFalse(self.widget._and_action.isEnabled())
+        self.assertFalse(self.widget._xor_action.isEnabled())
+        self.assertTrue(self.widget._new_action.isEnabled())
+        self.assertTrue(self.widget._copy_action.isEnabled())
+        self.assertTrue(self.widget._duplicate_action.isEnabled())
+        self.assertFalse(self.widget._paste_action.isEnabled())
+
+    def test_actions_enabled_single_data_selection(self):
+        layer = self.add_layer_via_method()
+        item = self.widget[layer]
+        self.widget.layerTree.setCurrentItem(item)
+
+        self.assertFalse(self.widget._or_action.isEnabled())
+        self.assertFalse(self.widget._and_action.isEnabled())
+        self.assertFalse(self.widget._xor_action.isEnabled())
+        self.assertTrue(self.widget._new_action.isEnabled())
+        self.assertFalse(self.widget._copy_action.isEnabled())
+        self.assertFalse(self.widget._duplicate_action.isEnabled())
+        self.assertFalse(self.widget._paste_action.isEnabled())
+
+    def test_actions_enabled_multi_subset_selection(self):
+        layer = self.setup_two_subset_selection()
+        self.assertTrue(self.widget._or_action.isEnabled())
+        self.assertTrue(self.widget._and_action.isEnabled())
+        self.assertTrue(self.widget._xor_action.isEnabled())
+        self.assertFalse(self.widget._new_action.isEnabled())
+        self.assertFalse(self.widget._copy_action.isEnabled())
+        self.assertFalse(self.widget._duplicate_action.isEnabled())
+        self.assertFalse(self.widget._paste_action.isEnabled())
+
+    def test_remove_all_layers(self):
+        for d in self.collect:
+            self.widget.add_layer(d)
+
+        self.widget.remove_all_layers()
+
+        for d in self.collect:
+            self.assertFalse(self.layer_present(d))
+
+    def test_checkable_toggle(self):
+        self.widget.set_checkable(True)
+        self.assertTrue(self.widget.is_checkable())
+        self.widget.set_checkable(False)
+        self.assertFalse(self.widget.is_checkable())
+
+    def test_remove_layer_keeps_layer_in_collection(self):
+        layer = self.add_layer_via_method()
+        self.widget.remove_layer(layer)
+        self.assertTrue(layer in self.widget.data_collection)
+
+    def test_remove_and_delete_removes_from_collection(self):
+        layer = self.add_layer_via_method()
+        item = self.widget[layer]
+        self.widget.layerTree.setCurrentItem(item)
+        self.widget.remove_and_delete_selected()
+        self.assertFalse(self.layer_present(layer))
+        self.assertFalse(layer in self.widget.data_collection)
+
+    def test_set_data_collection(self):
+        layer = self.add_layer_via_method()
+        dc = glue.DataCollection()
+        self.widget.data_collection = dc
+        self.assertTrue(self.widget.data_collection is dc)
+        self.assertFalse(self.layer_present(layer))
+
+    def test_edit_layer_label(self):
+        with patch('glue.qt.layertreewidget.qtutil') as util:
+            layer = self.add_layer_via_method()
+            item = self.widget[layer]
+            self.widget.layerTree.setCurrentItem(item, 0)
+            util.edit_layer_label = MagicMock()
+            self.widget.edit_current_layer()
+            util.edit_layer_label.assert_called_once_with(layer)
+
+    def test_edit_layer_color(self):
+        with patch('glue.qt.layertreewidget.qtutil') as util:
+            layer = self.add_layer_via_method()
+            item = self.widget[layer]
+            self.widget.layerTree.setCurrentItem(item, 1)
+            self.widget.edit_current_layer()
+            util.edit_layer_color.assert_called_once_with(layer)
+
+    def test_edit_layer_symbol(self):
+        with patch('glue.qt.layertreewidget.qtutil') as util:
+            layer = self.add_layer_via_method()
+            item = self.widget[layer]
+            self.widget.layerTree.setCurrentItem(item, 2)
+            self.widget.edit_current_layer()
+            util.edit_layer_symbol.assert_called_once_with(layer)
+
+    def test_edit_layer_point_size(self):
+        with patch('glue.qt.layertreewidget.qtutil') as util:
+            layer = self.add_layer_via_method()
+            item = self.widget[layer]
+            self.widget.layerTree.setCurrentItem(item, 3)
+            self.widget.edit_current_layer()
+            util.edit_layer_point_size.assert_called_once_with(layer)
+
+    def test_add_data_creates_default_label(self):
+        layer = self.data[0]
+        layer.style.label = None
+        self.widget.add_layer(layer)
+        self.assertTrue(layer in self.widget)
+        self.assertEquals(layer.style.label, "Data 0")
+
+    def test_load_data(self):
+        with patch('glue.qt.layertreewidget.qtutil') as util:
+            util.data_wizard.return_value = self.data[0]
+            self.widget._load_data()
+            self.assertTrue(self.layer_present(self.data[0]))
+
+    def test_load_data_doesnt_double_add(self):
+        "bugfix"
+        with patch('glue.qt.layertreewidget.qtutil') as util:
+            util.data_wizard.return_value = self.data[0]
+            self.assertEquals(self.widget.layerTree.topLevelItemCount(), 0)
+            self.widget._load_data()
+            self.assertEquals(self.widget.layerTree.topLevelItemCount(), 1)
+
+    def test_sync_external_layer_ignored(self):
+        self.assertFalse(self.data[0] in self.widget)
+        self.widget.sync_layer(self.data[0])
+
 
 if __name__ == "__main__":
     unittest.main()
