33869a2518df267ef16f9eec26f9cc5bb52d9f8b
Chris Beaumont <beaumont@hawaii.edu>
2012-06-18 10:25:05 -0400

    Multiple search paths for config.py. Issue #23

--- glue/config.py
+++ glue/config.py
@@ -1,22 +1,19 @@
 import os
 import imp
 
-# Set the list of configuration keys that we expect
 CONFIG_KEYS = ['extra_clients']
 
 def load_configuration():
     '''
-    Read in configuration settings from ~/.glue/config.py
+    Read in configuration settings from a config.py file.
+
+    Search order:
+
+     * current working directory
+     * environ var GLUERC
+     * HOME/.glue/config.py
     '''
-
-    # Find absolute path to configuration file
-    config_file = os.path.expanduser('~/.glue/config.py')
-
-    # Load the file
-    try:
-        config = imp.load_source('config', config_file)
-    except IOError:
-        config = None
+    config = _load_config_file()
 
     # Populate a configuration dictionary
     config_dict = {}
@@ -27,3 +24,24 @@ def load_configuration():
             config_dict[key] = None
 
     return config_dict
+
+def _load_config_file():
+    ''' Find and import a config.py file
+
+    Returns
+    -------
+    The module object, or None if no file found
+    '''
+    search_order = [os.path.join(os.getcwd(), 'config.py')]
+    if 'GLUERC' in os.environ:
+        search_order.append(os.environ['GLUERC'])
+    search_order.append(os.path.expanduser('~/glue/config.py'))
+
+    config = None
+    for config_file in search_order:
+        # Load the file
+        try:
+            config = imp.load_source('config', config_file)
+            return config
+        except IOError:
+            pass
