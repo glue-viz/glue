da67ec25208307b50c81628169db4af4c0bd7996
Thomas Robitaille <thomas.robitaille@gmail.com>
2012-07-05 09:33:20 +0200

    Moved last test to source tree

--- glue/qt/tests/test_application.py
+++ glue/qt/tests/test_application.py
@@ -1,15 +1,14 @@
-import unittest
-
 from PyQt4.QtGui import QApplication
 
-from glue.qt.glue_application import GlueApplication
+from ..glue_application import GlueApplication
 
 def tab_count(app):
     return app.tab_bar.count()
 
 
-class TestGlueApplication(unittest.TestCase):
-    def setUp(self):
+class TestGlueApplication(object):
+
+    def setup_method(self, method):
         self.qapp = QApplication([''])
         self.app = GlueApplication()
 
@@ -21,7 +20,4 @@ class TestGlueApplication(unittest.TestCase):
     def test_new_tabs(self):
         t0 = tab_count(self.app)
         self.app._new_tab()
-        self.assertEquals(t0+1, tab_count(self.app))
-
-if __name__ == "__main__":
-    unittest.main()
+        assert tab_count(self.app) == t0+1
