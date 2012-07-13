8bb355d0d13b64131e14ca57d7316fd59aa6ee00
Thomas Robitaille <thomas.robitaille@gmail.com>
2012-07-13 17:06:16 +0200

    Added some package-level imports for core, clients, and qt.widgets

---
+++ glue/qt/widgets/__init__.py
@@ -0,0 +1,7 @@
+from .custom_component_widget import CustomComponentWidget
+from .histogram_widget import HistogramWidget
+from .image_widget import ImageWidget
+from .layer_tree_widget import LayerTreeWidget
+from .message_widget import MessageWidget
+from .mpl_widget import MplWidget
+from .scatter_widget import ScatterWidget
