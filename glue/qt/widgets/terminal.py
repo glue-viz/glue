c7c56017b9d568f1fc73e4be226feb530fab0882
Chris Beaumont <beaumont@hawaii.edu>
2012-07-17 11:51:43 -0400

    terminal compatibility with IPython v0.12, v0.13. Closes #81

--- glue/qt/widgets/terminal.py
+++ glue/qt/widgets/terminal.py
@@ -3,124 +3,86 @@ A GUI Ipython terminal window which can interact
 with Glue. Based on code from
 
 http://stackoverflow.com/a/9796491/1332492
+and
+http://stackoverflow.com/a/11525205/1332492
 
 Usage:
-   new_widget = glue_terminal(data_collection)
+   new_widget = glue_terminal(**kwargs)
 """
-import os, sys
 import atexit
 
+from PyQt4 import QtCore
+
+from zmq import ZMQError
 from IPython.zmq.ipkernel import IPKernelApp
-from IPython.lib.kernel import find_connection_file, connect_qtconsole
+from IPython.lib.kernel import find_connection_file
 from IPython.frontend.qt.kernelmanager import QtKernelManager
 from IPython.frontend.qt.console.rich_ipython_widget import RichIPythonWidget
-from IPython.config.application import catch_config_error
-from PyQt4 import QtGui, QtCore
+from IPython.utils.traitlets import TraitError
 
+def default_kernel_app():
+    """ Return a configured IPKernelApp """
 
-class IPythonLocalKernelApp(IPKernelApp):
-    """IPython kernel application with nonblocking loop.
-        example:
-        app = QtGui.QApplication([])
-        kernelapp = IPythonLocalKernelApp.instance()
-        kernelapp.start()
-        namespace = kernelapp.get_user_namespace()
-        namespace["QtGui"]=QtGui
-        namespace["QtCore"]=QtCore
-        app.exec_()"""
-    #DEFAULT_INSTANCE_ARGS starting commandline
-    DEFAULT_INSTANCE_ARGS = ['qtconsole','--pylab=inline', '--colors=linux']
-
-    @catch_config_error
-    def initialize(self, argv=None):
-        super(IPythonLocalKernelApp, self).initialize(argv)
-        self.kernel.eventloop = self.loop_qt4_nonblocking
-
-    def loop_qt4_nonblocking(self, kernel):
-        """Non-blocking version of the ipython qt4 kernel loop"""
+    def event_loop(kernel):
+        """ Non-blocking qt event loop."""
         kernel.timer = QtCore.QTimer()
         kernel.timer.timeout.connect(kernel.do_one_iteration)
-        kernel.timer.start(1000*kernel._poll_interval)
+        kernel.timer.start(1000 * kernel._poll_interval)
 
-    def start(self, argv=DEFAULT_INSTANCE_ARGS):
-        """Starts IPython kernel app
-        argv: arguments passed to kernel
-        """
-        self.initialize(argv)
-        self.heartbeat.start()
-        if self.poller is not None:
-            self.poller.start()
+    app = IPKernelApp.instance()
+    try:
+        app.initialize(['python', '--pylab=qt'])
+    except ZMQError:
+        pass  # already set up
 
-        self.kernel.start()
+    app.kernel.eventloop = event_loop
 
-    def get_connection_file(self):
-        """Return current kernel connection file."""
-        return self.connection_file
+    try:
+        app.start()
+    except RuntimeError:  # already started
+        pass
 
-    def get_user_namespace(self):
-        """Returns current kernel userspace dict"""
-        return self.kernel.shell.user_ns
+    return app
 
-class IPythonConsoleQtWidget(RichIPythonWidget):
-    """Ipython console Qt4+ widget
-        Usage example:
-        app = QtGui.QApplication([])
-        kernelapp = IPythonLocalKernelApp.instance()
-        kernelapp.start()
-        namespace = kernelapp.get_user_namespace()
-        widget = IPythonConsoleQtWidget()
-        widget.set_default_style(colors='linux')
-        widget.connect_kernel(connection_file=kernelapp.get_connection_file())
-        # if you won't to connect to remote kernel:
-        widget.connect_kernel(connection_file='kernel-16098.json')
 
-        widget.show()
+def default_manager(kernel):
+    """ Return a configured QtKernelManager
 
-        namespace["widget"] = widget
-        namespace["QtGui"]=QtGui
-        namespace["QtCore"]=QtCore
+    :param kernel: An IPKernelApp instance
+    """
+    connection_file = find_connection_file(kernel.connection_file)
+    manager = QtKernelManager(connection_file=connection_file)
+    manager.load_connection_file()
+    manager.start_channels()
+    atexit.register(manager.cleanup_connection_file)
+    return manager
 
-        app.exec_()"""
-    _connection_file = None
-
-    def __init__(self, *args, **kw):
-        RichIPythonWidget.__init__(self, *args, **kw)
-        self._existing = True
-        self._may_close = False
-        self._confirm_exit = False
-
-    def _init_kernel_manager(self):
-        km = QtKernelManager(connection_file=self._connection_file, config=self.config)
-        km.load_connection_file()
-        km.start_channels(hb=self._heartbeat)
-        self.kernel_manager = km
-        atexit.register(self.kernel_manager.cleanup_connection_file)
-
-    def connect_kernel(self, connection_file, heartbeat=False):
-        """Connect's to ipython kernel.
-            connection_file    - connection file to use
-            heartbeat          - should start heartbeat server? Workaround for problems with inproc embedded kernels
-            (right click save image as/save as html kills kernel heartbeat/pool(??) serwer """
-        self._heartbeat = heartbeat
-        if os.path.exists(connection_file):
-            self._connection_file = connection_file
-        else:
-            self._connection_file = find_connection_file(connection_file)
-
-        self._init_kernel_manager()
 
 def glue_terminal(**kwargs):
-    from zmq import ZMQError
-    kernelapp = IPythonLocalKernelApp.instance()
-    try:
-        kernelapp.start()
-    except ZMQError:
-        pass # already started, no worries
-    namespace = kernelapp.get_user_namespace()
-    widget = IPythonConsoleQtWidget()
-    widget.gui_completion = True
-    widget.connect_kernel(connection_file=kernelapp.get_connection_file())
-    widget.show()
+    """ Return a qt widget which embed an IPython interpreter.
 
-    namespace.update(kwargs)
+    Extra keywords will be added to the namespace of the shell
+
+    :param kwargs: Extra variables to be added to the namespace
+
+    :rtype: QWidget
+    """
+
+    kernel_app = default_kernel_app()
+    manager = default_manager(kernel_app)
+
+    try:  # IPython v0.13
+        widget = RichIPythonWidget(gui_completion='droplist')
+    except TraitError:  # IPython v0.12
+        widget = RichIPythonWidget(gui_completion=True)
+    widget.kernel_manager = manager
+
+    # update namespace
+    kernel_app.shell.user_ns.update(kwargs)
+
+    # for debugging
+    kernel_app.shell.user_ns['_kernel'] = kernel_app
+    kernel_app.shell.user_ns['_manager'] = manager
+    kernel_app.shell.user_ns['_widget'] = glue_terminal
+
     return widget
