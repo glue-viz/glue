3048f048e2cfd54955c43bce41413c2005312e54
Chris Beaumont <beaumont@hawaii.edu>
2012-09-10 12:17:04 -0400

    qt decorators

---
+++ glue/qt/decorators.py
@@ -0,0 +1,21 @@
+from functools import wraps
+
+
+def set_cursor(shape):
+    """Set the Qt cursor for the duration of a function call, and unset
+
+    :param shape: Cursor shape to set.
+    """
+    def wrapper(func):
+        @wraps(func)
+        def result(*args, **kwargs):
+            from . import get_qapp
+            app = get_qapp()
+            app.setOverrideCursor(shape)
+            try:
+                func(*args, **kwargs)
+            finally:
+                app.restoreOverrideCursor()
+        return result
+
+    return wrapper
