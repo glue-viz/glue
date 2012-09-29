6b52dd0d031ac8ba9218a0ce9af715290852aa30
Chris Beaumont <beaumont@hawaii.edu>
2012-09-28 21:00:08 -0400

    change background color

--- glue/qt/widgets/glue_mdi_area.py
+++ glue/qt/widgets/glue_mdi_area.py
@@ -20,6 +20,7 @@ class GlueMdiArea(QtGui.QMdiArea):
         self._application = application
         self.setAcceptDrops(True)
         self.setAttribute(Qt.WA_DeleteOnClose)
+        self.setBackground(QtGui.QBrush(QtGui.QColor(250, 250, 250)))
 
     def dragEnterEvent(self, event):
         """ Accept the event if it has an application/py_instance format """
