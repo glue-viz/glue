665341ef4c4ff581194b7542a1b0463f6b01aba0
Chris Beaumont <beaumont@hawaii.edu>
2012-10-08 10:34:57 -0400

    split up data factory tests. PIL test skipped if PIL not present

--- glue/core/tests/test_data_factories.py
+++ glue/core/tests/test_data_factories.py
@@ -1,3 +1,4 @@
+import pytest
 from mock import MagicMock
 
 import glue.core.data_factories as df
@@ -18,14 +19,22 @@ def test_data_label():
     assert df.data_label('no_extension') == 'no_extension'
 
 
-def test_default_factories():
+def test_default_factory_gridded():
     for ext in ['fits', 'hdf5', 'hd5']:
         assert df.get_default_factory(ext) == df.gridded_data
 
-    for ext in ['vot', 'csv', 'txt', 'tsv']:
+
+def test_default_factory_tabular():
+    for ext in ['vot', 'csv', 'txt', 'tsv', 'xml']:
         assert df.get_default_factory(ext) == df.tabular_data
 
+
+#Factory is optional, and depends on PIL
+@pytest.mark.skipif("not hasattr(df, 'pil_data')")
+def test_default_factory_pil():
     for ext in ['png', 'jpeg', 'jpg', 'bmp', 'tiff']:
         assert df.get_default_factory(ext) == df.pil_data
 
+
+def test_default_factory_notfound():
     assert df.get_default_factory('weird_extension') is None
