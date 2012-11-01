a0766c7a12e48105b60cfbc16a7f411fb0a2ea3e
Chris Beaumont <beaumont@hawaii.edu>
2012-11-01 11:38:30 -0400

    More bugfixes when merging components with LinkSame

---
+++ glue/core/tests/test_links.py
@@ -0,0 +1,53 @@
+"""This file contains tests concerning linking data and accessing
+linked components"""
+
+from glue.core import Data, DataCollection
+from glue.core.link_helpers import LinkSame
+import numpy as np
+from numpy.random import random as r
+
+
+def test_1d_world_link():
+    x, y = r(10), r(10)
+    d1 = Data(label='d1', x=x)
+    d2 = Data(label='d2', y=y)
+    dc = DataCollection([d1, d2])
+
+    dc.add_link(LinkSame(d2.get_world_component_id(0), d1.id['x']))
+
+    assert d2.get_world_component_id(0) in d1.components
+    np.testing.assert_array_equal(d1[d2.get_world_component_id(0)], x)
+    np.testing.assert_array_equal(d1[d2.get_pixel_component_id(0)], x)
+
+
+def test_3d_world_link():
+    """Should be able to grab pixel coords after linking world"""
+    x, y, z = r(10), r(10), r(10)
+    cat = Data(label='cat', x=x, y=y, z=z)
+    im = Data(label='im', inten=r((3, 3, 3)))
+
+    dc = DataCollection([cat, im])
+
+    dc.add_link(LinkSame(im.get_world_component_id(2), cat.id['x']))
+    dc.add_link(LinkSame(im.get_world_component_id(1), cat.id['y']))
+    dc.add_link(LinkSame(im.get_world_component_id(0), cat.id['z']))
+
+    np.testing.assert_array_equal(cat[im.get_pixel_component_id(2)], x)
+    np.testing.assert_array_equal(cat[im.get_pixel_component_id(1)], y)
+    np.testing.assert_array_equal(cat[im.get_pixel_component_id(0)], z)
+
+
+def test_2d_world_link():
+    """Should be able to grab pixel coords after linking world"""
+
+    x, y = r(10), r(10)
+    cat = Data(label='cat', x=x, y=y)
+    im = Data(label='im', inten=r((3, 3)))
+
+    dc = DataCollection([cat, im])
+
+    dc.add_link(LinkSame(im.get_world_component_id(0), cat.id['x']))
+    dc.add_link(LinkSame(im.get_world_component_id(1), cat.id['y']))
+
+    np.testing.assert_array_equal(cat[im.get_pixel_component_id(0)], x)
+    np.testing.assert_array_equal(cat[im.get_pixel_component_id(1)], y)
