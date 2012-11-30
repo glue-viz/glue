51ca21651af16fedb442e736a3eba93250c4349b
Chris Beaumont <beaumont@hawaii.edu>
2012-11-30 16:42:44 -0500

    PEP8

--- glue/main.py
+++ glue/main.py
@@ -163,6 +163,7 @@ def start_glue(gluefile=None, config=None, datafiles=None):
     #QApplication.instance().processEvents()
     return ga.start()
 
+
 @die_on_error("Error running script")
 def execute_script(script):
     """ Run a python script and exit.
