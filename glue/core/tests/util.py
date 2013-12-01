2fceb47844c14a95be115780a002b0bd0707386d
Chris Beaumont <beaumont@hawaii.edu>
2013-12-01 17:38:49 -0600

    Command tests

--- glue/core/tests/util.py
+++ glue/core/tests/util.py
@@ -3,6 +3,11 @@ from contextlib import contextmanager
 import os
 import zlib
 
+from mock import MagicMock
+
+from ... import core
+from ...core.application_base import Application
+
 
 @contextmanager
 def make_file(contents, suffix, decompress=False):
@@ -22,3 +27,23 @@ def make_file(contents, suffix, decompress=False):
         yield fname
     finally:
         os.unlink(fname)
+
+
+@contextmanager
+def simple_catalog():
+    """Context manager to create a temporary data file
+
+    :param suffix: File suffix. string
+    """
+    with make_file('#a, b\n1, 2\n3, 4', '.csv') as result:
+        yield result
+
+
+def simple_session():
+    collect = core.data_collection.DataCollection()
+    hub = core.hub.Hub()
+    result = core.Session(data_collection=collect, hub=hub,
+                          application=MagicMock(Application),
+                          command_stack=core.CommandStack())
+    result.command_stack.session = result
+    return result
--- glue/qt/widgets/tests/__init__.py
+++ glue/qt/widgets/tests/__init__.py
@@ -1,9 +1,1 @@
-from .... import core
-
-
-def simple_session():
-    collect = core.data_collection.DataCollection()
-    hub = core.hub.Hub()
-    return core.Session(data_collection=collect, hub=hub,
-                        application=None,
-                        command_stack=core.CommandStack())
+from ....core.tests.util import simple_session
