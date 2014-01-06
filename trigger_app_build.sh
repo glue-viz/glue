5f0806bad87f965aa07f826fa7a742046c357de3
Chris Beaumont <beaumont@hawaii.edu>
2014-01-05 20:58:53 -0500

    Trigger App build from travis

---
+++ trigger_app_build.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+if ! [ $TRAVIS_REPO_SLUG ]; then
+    echo "Not building on travis. Exiting"
+    exit 0
+fi
+
+if [ $TRAVIS_REPO_SLUG != "glue-viz/glue" ]; then
+   echo "Not building from main repo. Exiting"
+   exit 0
+fi
+
+travis login --github-token=$GITHUB_TOKEN
+job_id=`travis branches -r glue-viz/Travis-MacGlue | grep $TRAVIS_BRANCH | cut -d" " -f 2-3 | cut -d '#' -f2`
+
+if ! [ $job_id ]; then
+   echo "Could not find a Travis-MacGlue branch named $TRAVIS_BRANCH. Exiting"
+   exit 0
+fi
+
+travis restart $job_id -r glue-viz/Travis-MacGlue
