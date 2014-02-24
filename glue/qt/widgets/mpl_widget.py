34117c7b271eb09fb551f0aa67ec6e2ae68e506c
Chris Beaumont <beaumont@hawaii.edu>
2014-02-24 11:23:08 -0500

    Cleanup matplotlib widget. Add a cooldown between canvas.draw operations
    This introduces a small delay in draw calls, but prevents quick
    updates from causing multiple intermediate draw requests

--- glue/qt/widgets/mpl_widget.py
+++ glue/qt/widgets/mpl_widget.py
@@ -1,32 +1,26 @@
 #!/usr/bin/env python
 
-# Python Qt4 bindings for GUI objects
 from ...external.qt import QtGui
 from ...external.qt.QtCore import Signal, Qt, QTimer
 
-# import the Qt4Agg FigureCanvas object, that binds Figure to
-# Qt4Agg backend. It also inherits from QWidget
 from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as \
     FigureCanvas
 
 try:
     from matplotlib.backends.backend_qt4agg import FigureManagerQTAgg as \
         FigureManager
-except ImportError:  # mpl >= 0.4
+except ImportError:  # mpl >= 1.4
     from matplotlib.backends.backend_qt4agg import FigureManagerQT as \
         FigureManager
 
 import matplotlib
 
-# Matplotlib Figure object
 from matplotlib.figure import Figure
 
 
 class MplCanvas(FigureCanvas):
-
     """Class to represent the FigureCanvas widget"""
 
-    # signals
     rightDrag = Signal(float, float)
     leftDrag = Signal(float, float)
     homeButton = Signal()
@@ -38,23 +32,17 @@ class MplCanvas(FigureCanvas):
         matplotlib.interactive(False)
         self.roi_callback = None
 
-        # setup Matplotlib Figure and Axis
         self.fig = Figure(facecolor='#ffffff')
-
         try:
             self.fig.set_tight_layout(True)
         except AttributeError:  # matplotlib < 1.1
             pass
 
-        # initialization of the canvas
         FigureCanvas.__init__(self, self.fig)
-
-        # we define the widget as expandable
         FigureCanvas.setSizePolicy(self,
                                    QtGui.QSizePolicy.Expanding,
                                    QtGui.QSizePolicy.Expanding)
 
-        # notify the system of updated policy
         FigureCanvas.updateGeometry(self)
         self.manager = FigureManager(self, 0)
         matplotlib.interactive(interactive)
@@ -63,6 +51,18 @@ class MplCanvas(FigureCanvas):
         self._resize_timer.setInterval(250)
         self._resize_timer.setSingleShot(True)
         self._resize_timer.timeout.connect(self._on_timeout)
+
+        self._draw_timer = QTimer()
+        self._draw_timer.setInterval(10)
+        self._draw_timer.setSingleShot(True)
+        self._draw_timer.timeout.connect(super(MplCanvas, self).draw)
+
+        self.renderer = None
+
+    def draw(self):
+        # delay a bit before actually drawing,
+        # to avoid multiple rapid draws
+        self._draw_timer.start()
 
     def _on_timeout(self):
         buttons =  QtGui.QApplication.instance().mouseButtons()
@@ -75,6 +75,11 @@ class MplCanvas(FigureCanvas):
         # draw the zoom rectangle more prominently
         drawRect = self.drawRect
         self.drawRect = False
+
+        # super needs this
+        if self.renderer is None:
+            self.renderer = self.get_renderer()
+
         super(MplCanvas, self).paintEvent(event)
         if drawRect:
             p = QtGui.QPainter(self)
