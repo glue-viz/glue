01c89ddba7de04b1cb43134b9581ba2a8cdf7e9a
Chris Beaumont <beaumont@hawaii.edu>
2014-03-11 16:08:41 -0400

    Cleanup spectrum tool code

--- glue/qt/spectrum_tool.py
+++ glue/qt/spectrum_tool.py
@@ -8,6 +8,49 @@ from .mouse_mode import SpectrumExtractorMode
 from ..core.callback_property import add_callback
 from .glue_toolbar import GlueToolbar
 
+
+class Extractor(object):
+
+    @staticmethod
+    def abcissa(data, axis):
+        slc = [0 for _ in data.shape]
+        slc[axis] = slice(None, None)
+        att = data.get_world_component_id(axis)
+        return data[att, tuple(slc)].ravel()
+
+    @staticmethod
+    def spectrum(data, attribute, roi, xaxis, yaxis, zaxis):
+        l, r, b, t = roi.xmin, roi.xmax, roi.ymin, roi.ymax
+        slc = [slice(None) for _ in data.shape]
+        slc[xaxis] = slice(l, r)
+        slc[yaxis] = slice(b, t)
+
+        x = Extractor.abcissa(data, zaxis)
+
+        data = data[attribute, tuple(slc)]
+        for i in reversed(list(range(data.ndim))):
+            if i != zaxis:
+                data = np.nansum(data, axis=i) / np.isfinite(data).sum(axis=i)
+
+        data = data.ravel()
+        return x, data
+
+    @staticmethod
+    def world2pixel(data, axis, value):
+        x = Extractor.abcissa(data, axis)
+        if x.size > 1 and (x[1] < x[0]):
+            x = x[::-1]
+            result = x.size - np.searchsorted(x, value) - 1
+        else:
+            result = np.searchsorted(x, value)
+        return np.clip(result, 0, x.size - 1)
+
+    @staticmethod
+    def pixel2world(data, axis, value):
+        x = Extractor.abcissa(data, axis)
+        return x[np.clip(value, 0, x.size - 1)]
+
+
 class SpectrumTool(object):
 
     def __init__(self, image_widget):
@@ -23,84 +66,129 @@ class SpectrumTool(object):
         self.axes = self.canvas.fig.add_subplot(111)
         self.profile = ProfileViewer(self.axes)
         self.mouse_mode = self._setup_mouse_mode()
+
+        self._last_profile_axis = None
+        self._relim_requested = False
+
         self._setup_handles()
         self._setup_toolbar()
 
     def _setup_mouse_mode(self):
         mode = SpectrumExtractorMode(self.image_widget.client.axes,
                                      release_callback=self._update_profile)
-        return mode
 
-    def _setup_handles(self):
-        self.slice_handle = self.profile.new_slider_handle(self._set_slice)
-        self.slicer_artist = SliderArtist(self.slice_handle)
-        set_slicer = lambda *args: setattr(self.slice_handle,
-                                           'value', self.current_channel)
-        add_callback(self.client, 'slice', set_slicer)
+        def toggle_mode_enabled(data):
+            mode.enabled = (data.ndim > 2)
+
+        add_callback(self.client, 'display_data', toggle_mode_enabled)
+        return mode
 
     def _setup_toolbar(self):
         tb = GlueToolbar(self.canvas, self.widget)
+
+        # disable ProfileViewer mouse processing during mouse modes
         tb.mode_activated.connect(self.profile.disconnect)
         tb.mode_deactivated.connect(self.profile.connect)
 
         self.widget.addToolBar(tb)
         return tb
 
+    def _setup_handles(self):
+
+        def _set_client_from_handle(value):
+            """Update client.slice given handle value"""
+            slc = list(self.client.slice)
+
+            # client.slice stored in pixel coords
+            value = Extractor.world2pixel(
+                self.data,
+                self.profile_axis, value)
+            slc[self.profile_axis] = value
+            self.client.slice = tuple(slc)
+
+        def _set_handle_from_client(slc):
+            """Update handle.value given client.slice"""
+            # handle.value is stored in world coordinates
+            val = slc[self.profile_axis]
+            val = Extractor.pixel2world(self.data, self.profile_axis, val)
+            self.slice_handle.value = val
+
+        self.slice_handle = self.profile.new_slider_handle()
+        self.slicer_artist = SliderArtist(self.slice_handle)
+
+        add_callback(self.client, 'slice', _set_handle_from_client)
+        add_callback(self.slice_handle, 'value', _set_client_from_handle)
+        add_callback(self.client, 'slice', self._check_invalidate)
+
+    def _check_invalidate(self, slc):
+        if self.profile_axis is None:
+            return
+
+        if self._last_profile_axis != self.profile_axis:
+            self._last_profile_axis = self.profile_axis
+            self.reset()
+
+    def reset(self):
+        self.hide()
+        self.mouse_mode.clear()
+        self._relim_requested = True
+
     @property
-    def current_channel(self):
-        s = self.client.slice
-        assert len(s) == 3
-        for ss in s:
-            if ss not in ['x', 'y']:
-                return ss
+    def data(self):
+        return self.client.display_data
+
+    @property
+    def profile_axis(self):
+        # XXX make this settable
+        # defaults to the non-xy axis with the most channels
+        slc = self.client.slice
+        candidates = [i for i, s in enumerate(slc) if s not in ['x', 'y']]
+        return max(candidates, key=lambda i: self.data.shape[i])
 
     def _recenter_handles(self):
         self.slice_handle.value = sum(self.axes.get_xlim()) / 2.
 
-    def _set_slice(self, value):
-        slc = list(self.client.slice)
-        assert len(slc) == 3
-
-        for i, s in enumerate(slc):
-            if s not in ['x', 'y']:
-                slc[i] = int(value)
-
-        self.client.slice = tuple(slc)
-
     def _update_profile(self, *args):
-        data = self.client.display_data
+        data = self.data
         att = self.client.display_attribute
+        slc = self.client.slice
+        roi = self.mouse_mode.roi()
+
+        xid = data.get_world_component_id(self.profile_axis)
+        units = data.get_component(xid).units
+        xlabel = str(xid) if units is None else '%s [%s]' % (xid, units)
+
         if data is None or att is None:
             return
 
-        roi = self.mouse_mode.roi()
-        x, y = self._extract_spectrum(roi, data, att)
+        xax = slc.index('x')
+        yax = slc.index('y')
+        zax = self.profile_axis
+
+        x, y = Extractor.spectrum(data, att, roi, xax, yax, zax)
+
         xlim = self.axes.get_xlim()
         self.profile.set_profile(x, y, c='k')
+
+        # relim x range if requested
+        if self._relim_requested:
+            self._relim_requested = False
+            self.axes.set_xlim(np.nanmin(x), np.nanmax(x))
+
+        # relim y range to data within the view window
+        mask = (self.axes.get_xlim()[0] <= x) & (x <= self.axes.get_xlim()[1])
+        ymask = y[mask]
+        self.axes.set_ylim(np.nanmin(ymask), np.nanmax(ymask))
+
         if self.axes.get_xlim() != xlim:
             self._recenter_handles()
+
+        self.axes.set_xlabel(xlabel)
+        self.axes.figure.canvas.draw()
         self.show()
-
-    def _extract_spectrum(self, roi, data, att):
-        #XXX this should be an roi/data method
-        #XXX assuming roi is rectangular
-        assert data.ndim == 3
-
-        l, r, b, t = roi.xmin, roi.xmax, roi.ymin, roi.ymax
-        slc = [slice(None) for _ in range(data.ndim)]
-        for i, s in enumerate(self.client.slice):
-            if s == 'x':
-                slc[i] = slice(l, r)
-            elif s == 'y':
-                slc[i] = slice(b, t)
-
-        data = data[tuple([att] + slc)]
-        for i, s in reversed(list(enumerate(self.client.slice))):
-            if s in ['x', 'y']:
-                data = np.nansum(data, axis=i) / np.isfinite(data).sum(axis=i)
-
-        data = data.ravel()
-        return np.arange(data.size), data
 
     def show(self):
         self.widget.show()
+
+    def hide(self):
+        self.widget.close()
---
+++ glue/qt/tests/test_profile_tool.py
@@ -0,0 +1,89 @@
+import numpy as np
+from mock import MagicMock
+
+from ...core.tests.util import simple_session
+from ...core import Data, Coordinates
+from ...core.roi import RectangularROI
+from ..widgets import ImageWidget
+
+from ..spectrum_tool import Extractor
+
+
+class TestCoordinates(Coordinates):
+
+    def pixel2world(self, *args):
+        return [a * 2 for a in args]
+
+    def world2pixel(self, *args):
+        return [a / 2 for a in args]
+
+
+class TestProfileTool(object):
+
+    def setup_method(self, method):
+        d = Data(x=np.zeros((3, 3, 3)))
+        session = simple_session()
+        session.data_collection.append(d)
+
+        self.image = ImageWidget(session)
+        self.image.add_data(d)
+        self.image.data = d
+        self.image.attribute = d.id['x']
+        self.tool = self.image._spectrum_tool
+        self.tool.show = lambda *args: None
+
+    def build_spectrum(self):
+        roi = RectangularROI()
+        roi.update_limits(0, 2, 0, 2)
+        self.tool._update_profile()
+
+    def test_reset_on_view_change(self):
+        self.build_spectrum()
+
+        self.tool.reset = MagicMock()
+        self.image.client.slice = ('x', 1, 'y')
+        assert self.tool.reset.call_count > 0
+
+
+class Test3DExtractor(object):
+
+    def setup_method(self, method):
+        self.data = Data()
+        self.data.coords = TestCoordinates()
+        self.data.add_component(np.ones((3, 4, 5)), label='x')
+
+    def test_abcissa(self):
+        expected = [0, 2, 4]
+        actual = Extractor.abcissa(self.data, 0)
+        np.testing.assert_equal(expected, actual)
+
+        expected = [0, 2, 4, 6]
+        actual = Extractor.abcissa(self.data, 1)
+        np.testing.assert_equal(expected, actual)
+
+        expected = [0, 2, 4, 6, 8]
+        actual = Extractor.abcissa(self.data, 2)
+        np.testing.assert_equal(expected, actual)
+
+    def test_spectrum(self):
+        roi = RectangularROI()
+        roi.update_limits(0, 0, 3, 3)
+
+        expected = [1, 1, 1]
+        _, actual = Extractor.spectrum(
+            self.data, self.data.id['x'], roi, 1, 2, 0)
+        np.testing.assert_array_equal(expected, actual)
+
+    def test_pixel2world(self):
+        assert Extractor.pixel2world(self.data, 0, 1) == 2
+
+        # clips to boundary
+        assert Extractor.pixel2world(self.data, 0, -1) == 0
+        assert Extractor.pixel2world(self.data, 0, 5) == 4
+
+    def test_world2pixel(self):
+        assert Extractor.world2pixel(self.data, 0, 2) == 1
+
+        # clips to boundary
+        assert Extractor.world2pixel(self.data, 0, -1) == 0
+        assert Extractor.world2pixel(self.data, 0, 8) == 2
