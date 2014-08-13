e7879fdb787c82b578df511ab054499f0ccead6c
Chris Beaumont <beaumont@hawaii.edu>
2014-08-13 17:36:29 -0400

    Version check for wcsaxes

--- glue/external/wcsaxes/__init__.py
+++ glue/external/wcsaxes/__init__.py
@@ -1,6 +1,6 @@
 from distutils.version import LooseVersion
 from astropy import __version__
-if LooseVersion(__version__) < LooseVersion('0.3.0'):
+if LooseVersion(__version__) < LooseVersion('0.3'):
     raise ImportError("wcsaxes requires astropy >= 0.3")
 
 from .wcsaxes import *
