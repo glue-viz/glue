a515ac4679772e9e7dd667853f8152a49f3e308e
Thomas Robitaille <thomas.robitaille@gmail.com>
2014-09-06 11:31:56 +0200

    Updated Miniconda to 3.6.0

--- .setup_conda.sh
+++ .setup_conda.sh
@@ -4,9 +4,9 @@ python_version=$1
 
 if [ ${python_version:0:1} == "2" ]
 then
-    wget http://repo.continuum.io/miniconda/Miniconda-3.3.0-Linux-x86_64.sh -O miniconda.sh
+    wget http://repo.continuum.io/miniconda/Miniconda-3.6.0-Linux-x86_64.sh -O miniconda.sh
 else
-    wget http://repo.continuum.io/miniconda/Miniconda3-3.3.0-Linux-x86_64.sh -O miniconda.sh
+    wget http://repo.continuum.io/miniconda/Miniconda3-3.6.0-Linux-x86_64.sh -O miniconda.sh
 fi
 
 chmod +x miniconda.sh
