7b54c54d8dfaca2e8fa9c07c78f52ff77f7fb8d7
Thomas Robitaille <thomas.robitaille@gmail.com>
2014-10-06 22:02:48 +0200

    Removed two more instances of requires_scipy

--- glue/clients/tests/test_util.py
+++ glue/clients/tests/test_util.py
@@ -3,16 +3,12 @@ from numpy.testing import assert_allclose
 
 from ..util import fast_limits
 
-from ...tests.helpers import requires_scipy
 
-
-@requires_scipy
 def test_fast_limits_nans():
     x = np.zeros((10, 10)) * np.nan
     assert_allclose(fast_limits(x, 0, 1), [0, 1])
 
 
-@requires_scipy
 def test_single_value():
     x = np.array([1])
     assert_allclose(fast_limits(x, 5., 95.), [1, 1])
