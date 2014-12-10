892027d2e82a80c97a181a5d0da2d8a57b047bab
Chris Beaumont <beaumont@hawaii.edu>
2014-12-10 09:52:46 -0500

    test fix

--- glue/core/tests/test_data_factories.py
+++ glue/core/tests/test_data_factories.py
@@ -181,7 +181,7 @@ def test_csv_pandas_factory():
     assert isinstance(d.get_component(cat_comp), CategoricalComponent)
     correct_cats = np.unique(np.asarray(['some', 'categorical',
                                          'data', 'here',
-                                         np.nan, np.nan, np.nan],
+                                         ''],
                                         dtype=np.object))
     np.testing.assert_equal(d.get_component(cat_comp)._categories,
                             correct_cats)
