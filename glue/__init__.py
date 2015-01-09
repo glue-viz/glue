e82da748cee71b742eb8e50818d165ce65caea68
Thomas Robitaille <thomas.robitaille@gmail.com>
2015-01-09 23:11:56 +0100

    Allow tests to be run with glue.test()

--- glue/__init__.py
+++ glue/__init__.py
@@ -123,3 +123,9 @@ from .qglue import qglue
 
 
 from .version import __version__
+
+def test():
+    import os
+    from pytest import main
+    root = os.path.abspath(os.path.dirname(__file__))
+    main(args=[root])
