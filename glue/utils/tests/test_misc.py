ee119cc9fd4e66d6014926647307db3edb2ddfa5
Thomas Robitaille <thomas.robitaille@gmail.com>
2015-01-26 22:55:23 +0100

    Added tests for glue.utils.misc

--- glue/utils/tests/test_misc.py
+++ glue/utils/tests/test_misc.py
@@ -1,4 +1,4 @@
-from ..misc import as_variable_name, file_format
+from ..misc import as_variable_name, file_format, DeferredMethod, nonpartial, lookup_class, as_list
 
 
 def test_as_variable_name():
@@ -37,3 +37,46 @@ class TestFileFormat(object):
     def test_nodot(self):
         fmt = file_format('test')
         assert fmt == ''
+
+
+def test_deferred_method():
+
+    class Test(object):
+
+        def __init__(self):
+            self.a = 1
+
+        def change_a(self):
+            self.a = 2
+
+    t = Test()
+
+    Test.change_a = DeferredMethod(Test.change_a)
+
+    t.change_a()
+
+    assert t.a == 1
+
+    Test.change_a.execute_deferred_calls()
+
+    assert t.a == 2
+
+
+def test_nonpartial():
+
+    def test(a=1, b=2):
+        pass
+
+    test_wrapped = nonpartial(test)
+    test_wrapped(a=1, b=2, c=3)
+
+
+def test_lookup_class():
+    lookup_class('glue.utils.misc.DeferredMethod') is DeferredMethod
+    lookup_class('gluh.utils.misc.DeferredMethod') is None
+    lookup_class('glue.utils.misc.DeferredMethods') is None
+
+
+def test_as_list():
+    as_list(1) == [1]
+    as_list([2, 3]) == [2, 3]
