64800639f4c86410590d9cd787b8f65018566fb5
Thomas Robitaille <thomas.robitaille@gmail.com>
2015-02-22 15:11:02 +0100

    Install astropy_helpers locally if needed

--- doc/conf.py
+++ doc/conf.py
@@ -21,6 +21,9 @@
 # If your documentation needs a minimal Sphinx version, state it here.
 #needs_sphinx = '1.0'
 
+from setuptools import Distribution
+Distribution({'setup_requires': 'astropy-helpers'})
+
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.todo', 'sphinx.ext.coverage',
