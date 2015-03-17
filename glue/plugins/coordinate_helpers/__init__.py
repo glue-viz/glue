f7e0716a936ae8a5e618dc3fec25a5fb11801584
Thomas Robitaille <thomas.robitaille@gmail.com>
2015-03-17 22:13:11 +0100

    Improved loading of coordinate_helpers module

--- glue/plugins/coordinate_helpers/__init__.py
+++ glue/plugins/coordinate_helpers/__init__.py
@@ -1,5 +1,9 @@
-try:
-    from .link_helpers import *
-except ImportError:
-    import warnings
-    warnings.warn("Could not import coordinate_helpers plugin, since Astropy>=0.4 is required")
+def setup():
+    from ...logger import logger
+    try:
+        from . import link_helpers
+    except ImportError:
+        import warnings
+        logger.info("Could not import coordinate_helpers plugin, since Astropy>=0.4 is required")
+    else:
+        logger.info("Loaded coordinate helpers plugin")
