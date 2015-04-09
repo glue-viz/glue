eb59124f394d03111b569d8bd277d386f5509113
Alex Koszycki <aak65@drexel.edu>
2015-04-09 18:08:42 -0400

    Cleaned up code!

--- glue/plugins/scatter_group/client.py
+++ glue/plugins/scatter_group/client.py
@@ -5,7 +5,7 @@ from pandas import DataFrame
 import numpy as np
 
 from ...external import six
-from ...utils import coerce_numeric, lookup_class
+from ...utils import coerce_numeric
 from ...core.data import IncompatibleAttribute, ComponentID, Component
 from ...core.callback_property import (CallbackProperty, add_callback)
 
@@ -14,189 +14,19 @@ from ...clients.scatter_client import ScatterClient
 from ...clients.layer_artist import (ChangedTrigger, ScatterLayerArtist)
 
 
-class ScatterGroupClient(ScatterClient):
-
-    """
-    A client class that uses matplotlib to visualize tables as scatter plots.
-    """
-    xmin = CallbackProperty(0)
-    xmax = CallbackProperty(1)
-    ymin = CallbackProperty(0)
-    ymax = CallbackProperty(1)
-    ylog = CallbackProperty(False)
-    xlog = CallbackProperty(False)
-    yflip = CallbackProperty(False)
-    xflip = CallbackProperty(False)
-    xatt = CallbackProperty()
-    yatt = CallbackProperty()
-    gatt = CallbackProperty()
-    jitter = CallbackProperty()
-
-    def _connect(self):
-        add_callback(self, 'xlog', self._set_xlog)
-        add_callback(self, 'ylog', self._set_ylog)
-        add_callback(self, 'xflip', self._set_limits)
-        add_callback(self, 'yflip', self._set_limits)
-        add_callback(self, 'xmin', self._set_limits)
-        add_callback(self, 'xmax', self._set_limits)
-        add_callback(self, 'ymin', self._set_limits)
-        add_callback(self, 'ymax', self._set_limits)
-        add_callback(self, 'xatt', partial(self._set_xydata, 'x'))
-        add_callback(self, 'yatt', partial(self._set_xydata, 'y'))
-        add_callback(self, 'gatt', partial(self._set_xydata, 'g'))
-        add_callback(self, 'jitter', self._jitter)
-        self.axes.figure.canvas.mpl_connect('draw_event',
-                                            lambda x: self._pull_properties())
-
-    def groupable_attributes(self, layer, show_hidden=False):
-        data = layer.data
-        l = data._shape[0]
-        if not data.find_component_id('None'):
-            none_comp = Component(np.array(range(0, l)), units='None')
-            data.add_component(none_comp, 'None', hidden=False)
-        else:
-            none_comp = data.find_component_id('None')
-            to_comp = coerce_numeric(np.array(range(0, l)))
-            to_comp.setflags(write=False)
-            none_comp._data = to_comp
-
-        comp = data.components if show_hidden else data.visible_components
-        groups = [comp[-1]]
-        for c in comp:
-            if data.get_component(c).group:
-                groups.append(c)
-        return groups
-
-    def add_layer(self, layer):
-        """ Adds a new visual layer to a client, to display either a dataset
-        or a subset. Updates both the client data structure and the
-        plot.
-
-        Returns the created layer artist
-
-        :param layer: the layer to add
-        :type layer: :class:`~glue.core.data.Data` or :class:`~glue.core.subset.Subset`
-        """
-        if layer.data not in self.data:
-            raise TypeError("Layer not in data collection")
-        if layer in self.artists:
-            return self.artists[layer][0]
-
-        result = ScatterGroupLayerArtist(layer, self.axes)
-        self.artists.append(result)
-        self._update_layer(layer)
-        self._ensure_subsets_added(layer)
-        return result
-
-    def _set_xydata(self, coord, attribute, snap=True):
-        """ Redefine which components get assigned to the x/y axes
-
-        :param coord: 'x' or 'y'
-           Which axis to reassign
-        :param attribute:
-           Which attribute of the data to use.
-        :type attribute: core.data.ComponentID
-        :param snap:
-           If True, will rescale x/y axes to fit the data
-        :type snap: bool
-        """
-
-        if coord not in ('x', 'y', 'g'):
-            raise TypeError("coord must be one of x, y, g")
-        if not isinstance(attribute, ComponentID):
-            raise TypeError("attribute must be a ComponentID")
-
-        # update coordinates of data and subsets
-        if coord == 'x':
-            new_add = not self._xset
-            self.xatt = attribute
-            self._xset = self.xatt is not None
-        elif coord == 'y':
-            new_add = not self._yset
-            self.yatt = attribute
-            self._yset = self.yatt is not None
-        elif coord == 'g':
-             self.gatt = attribute
-             self._gset = self.gatt is not None
-
-        # update plots
-        list(map(self._update_layer, self.artists.layers))
-
-        if coord == 'x' and snap:
-            self._snap_xlim()
-            if new_add:
-                self._snap_ylim()
-        elif coord == 'y' and snap:
-            self._snap_ylim()
-            if new_add:
-                self._snap_xlim()
-
-        self._update_axis_labels()
-        self._pull_properties()
-        self._redraw()
-
-    def restore_layers(self, layers, context):
-        """ Re-generate a list of plot layers from a glue-serialized list"""
-        for l in layers:
-            cls = lookup_class(l.pop('_type'))
-            if cls != ScatterGroupLayerArtist:
-                raise ValueError("Scatter client cannot restore layer of type "
-                                 "%s" % cls)
-            props = dict((k, context.object(v)) for k, v in l.items())
-            layer = self.add_layer(props['layer'])
-            layer.properties = props
-
-    def _update_layer(self, layer, force=False):
-        """ Update both the style and data for the requested layer"""
-        if self.xatt is None or self.yatt is None:
-            return
-
-        if layer not in self.artists:
-            return
-
-        self._layer_updated = True
-        for art in self.artists[layer]:
-            art.xatt = self.xatt
-            art.yatt = self.yatt
-            art.gatt = self.gatt
-            art.force_update() if force else art.update()
-        self._redraw()
-
-    def _on_component_replace(self, msg):
-        old = msg.old
-        new = msg.new
-
-        if self.xatt is old:
-            self.xatt = new
-        if self.yatt is old:
-            self.yatt = new
-        if self.gatt is old:
-            self.gatt = new
-
-
 @six.add_metaclass(ABCMeta)
 class ScatterGroupLayerBase(object):
-
-    # which ComponentID to assign to X axis
     xatt = abstractproperty()
-
-    # which ComponentID to assign to Y axis
     yatt = abstractproperty()
-
-    # which ComponentID to group by
     gatt = abstractproperty()
 
     @abstractmethod
     def get_data(self):
-        """
-        Return the scatterpoint data as an (N, 2) array
-        """
         pass
+
 
 class ScatterGroupLayerArtist(ScatterLayerArtist, ScatterGroupLayerBase):
 
-    xatt = ChangedTrigger()
-    yatt = ChangedTrigger()
     gatt = ChangedTrigger()
     _property_set = ScatterLayerArtist._property_set + ['gatt']
 
@@ -230,10 +60,67 @@ class ScatterGroupLayerArtist(ScatterLayerArtist, ScatterGroupLayerBase):
 
         df = DataFrame({'g': g, 'x': x, 'y': y})
         groups = df.groupby('g')
-        if int(len(groups)) < int(len(x)):
+        if len(groups) < len(x):
             colors = get_colors(len(groups))
             for grp, c in zip(groups, colors):
                 art = self._axes.plot(grp[1]['x'], grp[1]['y'], '.-', color=c)
                 self.artists.extend(art)
         return True
+
+
+class ScatterGroupClient(ScatterClient):
+    gatt = CallbackProperty()
+    layer_artist_class = ScatterGroupLayerArtist
+
+    def _connect(self):
+        add_callback(self, 'gatt', partial(self._set_xydata, 'g'))
+        super(ScatterGroupClient, self)._connect()
+
+    def groupable_attributes(self, layer, show_hidden=False):
+        data = layer.data
+        l = data._shape[0]
+        if not data.find_component_id('None'):
+            none_comp = Component(np.array(range(0, l)), units='None')
+            data.add_component(none_comp, 'None', hidden=False)
+        else:
+            none_comp = data.find_component_id('None')
+            to_comp = coerce_numeric(np.array(range(0, l)))
+            to_comp.setflags(write=False)
+            none_comp._data = to_comp
+
+        comp = data.components if show_hidden else data.visible_components
+        groups = [comp[-1]]
+        for c in comp:
+            if data.get_component(c).group:
+                groups.append(c)
+        return groups
+
+    def _set_xydata(self, coord, attribute, snap=True):
+        if coord not in ('x', 'y', 'g'):
+            raise TypeError("coord must be one of x, y, g")
+        if not isinstance(attribute, ComponentID):
+            raise TypeError("attribute must be a ComponentID")
+        if coord == 'g':
+            self.gatt = attribute
+            self._gset = self.gatt is not None
+            list(map(self._update_layer, self.artists.layers))
+            self._update_axis_labels()
+            self._pull_properties()
+            self._redraw()
+        else:
+            super(ScatterGroupClient, self)._set_xydata(coord, attribute, snap)
+
+    def _update_layer(self, layer, force=False):
+        """ Update both the style and data for the requested layer"""
+        for art in self.artists[layer]:
+            art.gatt = self.gatt
+        super(ScatterGroupClient, self)._update_layer(layer, force)
+
+    def _on_component_replace(self, msg):
+        old = msg.old
+        new = msg.new
+        if self.gatt is old:
+            self.gatt = new
+        super(ScatterGroupClient, self)._on_component_replace(msg)
+
 
--- glue/plugins/scatter_group/qt_widget.py
+++ glue/plugins/scatter_group/qt_widget.py
@@ -3,11 +3,9 @@ import os.path
 
 from ...qt.widgets.scatter_widget import ScatterWidget
 from ...qt.widgets.mpl_widget import defer_draw
-from ...qt.widget_properties import (ButtonProperty, FloatLineProperty,
-                                 CurrentComboProperty,
-                                 connect_bool_button, connect_float_edit)
+from ...qt.widget_properties import CurrentComboProperty
 
-from ...qt.qtutil import nonpartial, load_ui
+from ...qt.qtutil import load_ui
 from .client import ScatterGroupClient
 
 __all__ = ['ScatterGroupWidget']
@@ -17,20 +15,6 @@ class ScatterGroupWidget(ScatterWidget):
 
     LABEL = "Scatter Group Plot"
     _property_set = ScatterWidget._property_set + ['gatt']
-
-    xlog = ButtonProperty('ui.xLogCheckBox', 'log scaling on x axis?')
-    ylog = ButtonProperty('ui.yLogCheckBox', 'log scaling on y axis?')
-    xflip = ButtonProperty('ui.xFlipCheckBox', 'invert the x axis?')
-    yflip = ButtonProperty('ui.yFlipCheckBox', 'invert the y axis?')
-    xmin = FloatLineProperty('ui.xmin', 'Lower x limit of plot')
-    xmax = FloatLineProperty('ui.xmax', 'Upper x limit of plot')
-    ymin = FloatLineProperty('ui.ymin', 'Lower y limit of plot')
-    ymax = FloatLineProperty('ui.ymax', 'Upper y limit of plot')
-    hidden = ButtonProperty('ui.hidden_attributes', 'Show hidden attributes')
-    xatt = CurrentComboProperty('ui.xAxisComboBox',
-                                'Attribute to plot on x axis')
-    yatt = CurrentComboProperty('ui.yAxisComboBox',
-                                'Attribute to plot on y axis')
     gatt = CurrentComboProperty('ui.groupComboBox',
                                 'Attribute to group time series')
 
@@ -43,100 +27,36 @@ class ScatterGroupWidget(ScatterWidget):
                                          artist_container=self._container)
 
     def _connect(self):
-         ui = self.ui
-         cl = self.client
-
-         connect_bool_button(cl, 'xlog', ui.xLogCheckBox)
-         connect_bool_button(cl, 'ylog', ui.yLogCheckBox)
-         connect_bool_button(cl, 'xflip', ui.xFlipCheckBox)
-         connect_bool_button(cl, 'yflip', ui.yFlipCheckBox)
-
-         ui.xAxisComboBox.currentIndexChanged.connect(self.update_xatt)
-         ui.yAxisComboBox.currentIndexChanged.connect(self.update_yatt)
-         ui.groupComboBox.currentIndexChanged.connect(self.update_gatt)
-         ui.hidden_attributes.toggled.connect(lambda x: self._update_combos())
-         ui.swapAxes.clicked.connect(nonpartial(self.swap_axes))
-         ui.snapLimits.clicked.connect(cl.snap)
-
-         connect_float_edit(cl, 'xmin', ui.xmin)
-         connect_float_edit(cl, 'xmax', ui.xmax)
-         connect_float_edit(cl, 'ymin', ui.ymin)
-         connect_float_edit(cl, 'ymax', ui.ymax)
+        ui = self.ui
+        ui.groupComboBox.currentIndexChanged.connect(self.update_gatt)
+        super(ScatterGroupWidget, self)._connect()
 
     @defer_draw
     def _update_combos(self):
-        """ Update contents of combo boxes """
-
-        # have to be careful here, since client and/or widget
-        # are potentially out of sync
-
-        layer_ids = []
+        super(ScatterGroupWidget, self)._update_combos()
         glayer_ids = []
-
-        # show hidden attributes if needed
-        if ((self.client.xatt and self.client.xatt.hidden) or
-                (self.client.yatt and self.client.yatt.hidden) or
-                (self.client.gatt and self.client.gatt.hidden)):
+        if (self.client.gatt and self.client.gatt.hidden):
             self.hidden = True
-
-        # determine which components to put in combos
         for l in self.client.data:
             if not self.client.is_layer_present(l):
                 continue
-            for lid in self.client.plottable_attributes(
+            for glid in self.client.groupable_attributes(
                     l, show_hidden=self.hidden):
-                if lid not in layer_ids:
-                    layer_ids.append(lid)
-                for glid in self.client.groupable_attributes(
-                        l, show_hidden=self.hidden):
-                    if glid not in glayer_ids:
-                        glayer_ids.append(glid)
+                if glid not in glayer_ids:
+                    glayer_ids.append(glid)
 
-        oldx = self.xatt
-        oldy = self.yatt
         oldg = self.gatt
-        newx = self.client.xatt or oldx
-        newy = self.client.yatt or oldy
         newg = self.client.gatt or oldg
-
-        for combo, target in zip([self.ui.xAxisComboBox, self.ui.yAxisComboBox],
-                                 [newx, newy]):
-            combo.blockSignals(True)
-            combo.clear()
-
-            if not layer_ids:  # empty component list
-                continue
-
-            # populate
-            for lid in layer_ids:
-               combo.addItem(lid.label, userData=lid)
-
-            idx = layer_ids.index(target) if target in layer_ids else 0
-            combo.setCurrentIndex(idx)
-
-            combo.blockSignals(False)
-
         for combo, target in zip([self.ui.groupComboBox], [newg]):
-
             combo.blockSignals(True)
             combo.clear()
-
-            if not glayer_ids:  # empty component list
+            if not glayer_ids:
                 continue
-
-            # populate
             for glid in glayer_ids:
                 combo.addItem(glid.label, userData=glid)
-
             idx = glayer_ids.index(target) if target in glayer_ids else 0
             combo.setCurrentIndex(idx)
-
             combo.blockSignals(False)
-
-        # ensure client and widget synced
-        self.client.xatt = self.xatt
-        self.client.lyatt = self.yatt
-        self.client.yatt = self.yatt
         self.client.gatt = self.gatt
 
     @defer_draw
@@ -146,9 +66,5 @@ class ScatterGroupWidget(ScatterWidget):
 
     @defer_draw
     def restore_layers(self, rec, context):
-        self.client.restore_layers(rec, context)
-        self._update_combos()
-        # manually force client attributes to sync
-        self.update_xatt(None)
-        self.update_yatt(None)
+        super(ScatterGroupWidget, self).restore_layers(rec, context)
         self.update_gatt(None)
