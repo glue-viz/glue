90d6a383b2b44dfa9e37008622456400b6676ae9
Thomas Robitaille <thomas.robitaille@gmail.com>
2015-08-15 22:02:38 +0200

    Fix tests when astrodendro is not installed

--- glue/core/data_factories/tests/test_dendro_loader.py
+++ glue/core/data_factories/tests/test_dendro_loader.py
@@ -4,7 +4,6 @@ import os
 
 import pytest
 
-from ..dendro_loader import is_dendro, load_dendro
 from ..helpers import find_factory
 from ....tests.helpers import requires_astrodendro
 
@@ -16,10 +15,12 @@ DATA = os.path.join(os.path.dirname(__file__), 'data')
 @requires_astrodendro
 @pytest.mark.parametrize('filename', ['dendro.fits', 'dendro_old.fits', 'dendro.hdf5'])
 def test_is_dendro(filename):
+    from ..dendro_loader import is_dendro
     assert is_dendro(os.path.join(DATA, filename))
 
 
 @requires_astrodendro
 @pytest.mark.parametrize('filename', ['dendro.fits', 'dendro_old.fits', 'dendro.hdf5'])
 def test_find_factory(filename):
+    from ..dendro_loader import load_dendro
     assert find_factory(os.path.join(DATA, filename)) is load_dendro
