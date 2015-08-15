22210565b2b2ec9f1d0238a2c9aaff8cda62215a
Thomas Robitaille <thomas.robitaille@gmail.com>
2015-08-15 21:05:12 +0200

    Skip dendrogram tests if astrodendro is not present

--- glue/core/data_factories/tests/test_dendro_loader.py
+++ glue/core/data_factories/tests/test_dendro_loader.py
@@ -6,16 +6,20 @@ import pytest
 
 from ..dendro_loader import is_dendro, load_dendro
 from ..helpers import find_factory
+from ....tests.helpers import requires_astrodendro
+
 
 
 DATA = os.path.join(os.path.dirname(__file__), 'data')
 
 
+@requires_astrodendro
 @pytest.mark.parametrize('filename', ['dendro.fits', 'dendro_old.fits', 'dendro.hdf5'])
 def test_is_dendro(filename):
     assert is_dendro(os.path.join(DATA, filename))
 
 
+@requires_astrodendro
 @pytest.mark.parametrize('filename', ['dendro.fits', 'dendro_old.fits', 'dendro.hdf5'])
 def test_find_factory(filename):
     assert find_factory(os.path.join(DATA, filename)) is load_dendro
