ca4b5b81445d4c7de982a7405f396a1508330f9d
Thomas Robitaille <thomas.robitaille@gmail.com>
2015-08-15 17:27:27 +0200

    Minor fix to dendrogram try...except

--- glue/core/data_factories/dendrogram.py
+++ glue/core/data_factories/dendrogram.py
@@ -6,9 +6,10 @@ __all__ = []
 
 try:
     from .dendro_loader import load_dendro, is_dendro
+except ImportError:
+    pass
+else:
     __factories__.append(load_dendro)
     load_dendro.label = 'Dendrogram'
     load_dendro.identifier = is_dendro
     load_dendro.priority = 1000
-except ImportError:
-    pass
