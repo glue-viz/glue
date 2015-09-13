36f0cdef11fe76fd02473fdf2888cc081c1b1610
Thomas Robitaille <thomas.robitaille@gmail.com>
2015-09-13 15:48:21 +0200

    Re-arrange tests

--- glue/core/data_factories/__init__.py
+++ glue/core/data_factories/__init__.py
@@ -1,7 +1,8 @@
 from __future__ import absolute_import, division, print_function
 
 from .helpers import *
-from .containers import *
+from .fits import *
+from .hdf5 import *
 from .gridded import *
 from .pandas import *
 from .excel import *
