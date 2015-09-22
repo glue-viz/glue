caf07f8517a2aa5c20970d0464af80f5ed1841df
Thomas Robitaille <thomas.robitaille@gmail.com>
2015-09-22 23:36:23 +0200

    Make TableWidget fully functional

--- glue/qt/widgets/table_widget.py
+++ glue/qt/widgets/table_widget.py
@@ -2,8 +2,14 @@ from __future__ import absolute_import, division, print_function
 
 from .data_viewer import DataViewer
 
-from ...external.qt.QtGui import QTableView
-from ...external.qt.QtCore import Qt, QAbstractTableModel
+from ...external.qt.QtGui import QTableView, QAbstractItemView, QItemSelectionModel, QItemSelection
+from ...external.qt.QtCore import Qt, QAbstractTableModel, QAbstractItemModel, SIGNAL, QObject
+
+from ...core import message as msg
+from ...core.edit_subset_mode import EditSubsetMode
+from ...core.subset import ElementSubsetState
+
+from ..qtutil import load_ui
 
 import numpy as np
 
@@ -44,40 +50,113 @@ class DataTableModel(QAbstractTableModel):
         if role == Qt.DisplayRole:
             c = self.columns[index.column()]
             idx = np.unravel_index([index.row()], self._data.shape)
-            return str(self._data[c, idx][0])
+            comp = self._data.get_component(c)
+            if comp.categorical:
+                comp = comp.labels
+            else:
+                comp = comp.data
+            return str(comp[idx][0])
 
 
 class TableWidget(DataViewer):
-    def __init__(self, session, parent=None):
-        super(TableWidget, self).__init__(session, parent)
-        self.widget = QTableView()
-        self.setCentralWidget(self.widget)
 
-        hdr = self.widget.horizontalHeader()
+    LABEL = "Table Viewer"
+
+    def __init__(self, session, parent=None, widget=None):
+
+        super(TableWidget, self).__init__(session, parent)
+
+        self.ui = load_ui('table_widget')
+        self.setCentralWidget(self.ui)
+
+        hdr = self.ui.table.horizontalHeader()
         hdr.setStretchLastSection(True)
         hdr.setResizeMode(hdr.Interactive)
 
-        hdr = self.widget.verticalHeader()
+        hdr = self.ui.table.verticalHeader()
         hdr.setResizeMode(hdr.Interactive)
 
-    def __str__(self):
-        return "Table Widget"
+
+        self.ui.table.clicked.connect(self._clicked)
+
+        self.model = None
+
+    def register_to_hub(self, hub):
+
+        super(TableWidget, self).register_to_hub(hub)
+
+        dfilter = lambda x: True
+        dcfilter = lambda x: True
+        subfilter = lambda x: True
+
+        hub.subscribe(self, msg.SubsetCreateMessage,
+                      handler=self._add_subset,
+                      filter=dfilter)
+
+        hub.subscribe(self, msg.SubsetUpdateMessage,
+                      handler=self._update_subset,
+                      filter=subfilter)
+
+        hub.subscribe(self, msg.SubsetDeleteMessage,
+                      handler=self._remove_subset)
+
+        hub.subscribe(self, msg.DataUpdateMessage,
+                      handler=self.update_window_title)
+
+    def _clicked(self, mode):
+        self._broadcast_selection()
+
+    def _broadcast_selection(self):
+
+        if self.data is not None:
+
+            model = self.ui.table.selectionModel()
+            rows = [x.row() for x in model.selectedRows()]
+            subset_state = ElementSubsetState(rows)
+
+            mode = EditSubsetMode()
+            mode.update(self.data, subset_state, focus_data=self.data)
+
+    def _add_subset(self, message):
+
+        self.subset = message.subset
+        self.ui.table.clearSelection()
+        selection_mode = self.ui.table.selectionMode()
+        self.ui.table.setSelectionMode(QAbstractItemView.MultiSelection);
+
+        # The following is more efficient than just calling selectRow
+        model = self.ui.table.selectionModel()
+        for index in message.subset.to_index_list():
+            model_index = self.model.createIndex(index, 0)
+            model.select(model_index,
+                         QItemSelectionModel.Select | QItemSelectionModel.Rows)
+
+        self.ui.table.setSelectionMode(selection_mode)
+
+    def _update_subset(self, message):
+        self._add_subset(message)
+
+    def _remove_subset(self, message):
+        self.ui.table.clearSelection()
+
+    def _update_data(self, message):
+        self.set_data(message.data)
 
     def unregister(self, hub):
         pass
 
     def add_data(self, data):
+        self.data = data
         self.set_data(data)
         return True
 
     def add_subset(self, subset):
-        self.set_data(subset.data)
         return True
 
     def set_data(self, data):
         self.setUpdatesEnabled(False)
-        model = DataTableModel(data)
-        self.widget.setModel(model)
+        self.model = DataTableModel(data)
+        self.ui.table.setModel(self.model)
         self.setUpdatesEnabled(True)
 
     def closeEvent(self, event):
@@ -88,5 +167,5 @@ class TableWidget(DataViewer):
         """
         from ...core import Data
         d = Data(x=[0])
-        self.widget.setModel(DataTableModel(d))
+        self.ui.table.setModel(DataTableModel(d))
         event.accept()
