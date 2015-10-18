417bcc4fb8e017e0b32018949c68f35a625cb8b5
Thomas Robitaille <thomas.robitaille@gmail.com>
2015-10-18 18:50:25 +0200

    Re-organized Astropy-based readers to include a fallback reader that tries to read in any file that failed otherwise.

--- glue/core/data_factories/tables.py
+++ glue/core/data_factories/tables.py
@@ -12,25 +12,37 @@ __all__ = ['tabular_data', 'sextractor_factory', 'astropy_tabular_data',
            'formatted_table_factory']
 
 
-def _ascii_identifier_v02(origin, args, kwargs):
-    # this works for astropy v0.2
-    if isinstance(args[0], six.string_types):
-        return args[0].endswith(('csv', 'tsv', 'txt', 'tbl', 'dat',
-                                 'csv.gz', 'tsv.gz', 'txt.gz', 'tbl.gz',
-                                 'dat.gz'))
-    else:
+def is_astropy_tabular_data(filename, **kwargs):
+    try:
+        table = astropy_table_read(filename, **kwargs)
+    except:
         return False
+    else:
+        return True
 
 
-def _ascii_identifier_v03(origin, *args, **kwargs):
-    # this works for astropy v0.3
-    return _ascii_identifier_v02(origin, args, kwargs)
+def astropy_table_read(*args, **kwargs):
+
+    from astropy.table import Table
+
+    # In Python 3, as of Astropy 0.4, if the format is not specified, the
+    # automatic format identification will fail (astropy/astropy#3013).
+    # This is only a problem for ASCII formats however, because it is due
+    # to the fact that the file object in io.ascii does not rewind to the
+    # start between guesses (due to a bug), so here we can explicitly try
+    # the ASCII format if the format keyword was not already present. But 
+    # also more generally, we should first try the ASCII readers.
+    if 'format' not in kwargs:
+        try:
+            return Table.read(*args, format='ascii', **kwargs)
+        except:
+            pass
+
+    # If the above didn't work, attempt to read with no specified format
+    return Table.read(*args, **kwargs)
 
 
-@data_factory(label="Catalog (Astropy Parser)",
-              identifier=has_extension('xml vot csv txt tsv tbl dat fits '
-                                       'xml.gz vot.gz csv.gz txt.gz tbl.bz '
-                                       'dat.gz fits.gz'))
+@data_factory(label="Catalog (Astropy Parser)", identifier=is_astropy_tabular_data)
 def astropy_tabular_data(*args, **kwargs):
     """
     Build a data set from a table. We restrict ourselves to tables
@@ -39,49 +51,10 @@ def astropy_tabular_data(*args, **kwargs):
     All arguments are passed to
         astropy.table.Table.read(...).
     """
-    from distutils.version import LooseVersion
-    from astropy import __version__
-    if LooseVersion(__version__) < LooseVersion("0.2"):
-        raise RuntimeError("Glue requires astropy >= v0.2. Please update")
 
     result = Data()
 
-    # Read the table
-    from astropy.table import Table
-
-    # Add identifiers for ASCII data
-    from astropy.io import registry
-    if LooseVersion(__version__) < LooseVersion("0.3"):
-        registry.register_identifier('ascii', Table, _ascii_identifier_v02,
-                                     force=True)
-    else:
-        # Basically, we always want the plain ascii reader for now.
-        # But astropy will complain about ambiguous formats (or use another reader)
-        # unless we remove other registry identifiers and set up our own reader
-
-        nope = lambda *a, **k: False
-        registry.register_identifier('ascii.glue', Table, _ascii_identifier_v03,
-                                     force=True)
-        registry.register_identifier('ascii.csv', Table, nope, force=True)
-        registry.register_identifier('ascii.fast_csv', Table, nope, force=True)
-        registry.register_identifier('ascii', Table, nope, force=True)
-        registry.register_reader('ascii.glue', Table,
-                                 lambda path: Table.read(path, format='ascii'),
-                                 force=True)
-
-    try:
-        table = Table.read(*args, **kwargs)
-    except:
-        # In Python 3, as of Astropy 0.4, if the format is not specified, the
-        # automatic format identification will fail (astropy/astropy#3013).
-        # This is only a problem for ASCII formats however, because it is due
-        # to the fact that the file object in io.ascii does not rewind to the
-        # start between guesses (due to a bug), so here we can explicitly try
-        # the ASCII format if the format keyword was not already present.
-        if 'format' not in kwargs:
-            table = Table.read(*args, format='ascii.glue', **kwargs)
-        else:
-            raise
+    table = astropy_table_read(*args, **kwargs)
 
     # Loop through columns and make component list
     for column_name in table.columns:
@@ -122,11 +95,11 @@ def tabular_data(path, **kwargs):
 
 
 def formatted_table_factory(format, label):
-    
+
     @data_factory(label=label, identifier=lambda *a, **k: False)
     def factory(file, **kwargs):
         kwargs['format'] = 'ascii.%s' % format
-        return tabular_data(file, **kwargs)
+        return astropy_tabular_data(file, **kwargs)
 
     # rename function to its variable reference below
     # allows pickling to work
