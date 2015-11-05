1c469fab397b6c282595e61ef7829cae2961ade7
Brigitta Sipocz <bsipocz@gmail.com>
2015-11-05 00:26:00 +0000

    Updating travis to reflect recent miniconda changes, and to use the latest miniconda script

--- continuous-integration/travis/install_conda_osx.sh
+++ continuous-integration/travis/install_conda_osx.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-wget http://repo.continuum.io/miniconda/Miniconda-3.7.3-MacOSX-x86_64.sh -O miniconda.sh
-chmod +x miniconda.sh
-./miniconda.sh -b
-export PATH=/Users/travis/miniconda/bin:$PATH
-conda update --yes conda
+# http://conda.pydata.org/docs/travis.html#the-travis-yml-file
+wget http://repo.continuum.io/miniconda/Miniconda-latest-MacOSX-x86_64.sh -O miniconda.sh
+
+bash miniconda.sh -b -p $HOME/miniconda
+export PATH="$HOME/miniconda/bin:$PATH"
