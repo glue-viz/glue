1811198fcb53f0f1ab14084f56fd169518c43926
Thomas Robitaille <thomas.robitaille@gmail.com>
2015-11-13 17:21:18 +0000

    Try using ci-helpers from Astropy project

--- continuous-integration/travis/install_conda_linux.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# http://conda.pydata.org/docs/travis.html#the-travis-yml-file
-wget http://repo.continuum.io/miniconda/Miniconda-latest-Linux-x86_64.sh -O miniconda.sh
-bash miniconda.sh -b -p $HOME/miniconda
-export PATH="$HOME/miniconda/bin:$PATH"
--- continuous-integration/travis/install_conda_osx.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# http://conda.pydata.org/docs/travis.html#the-travis-yml-file
-wget http://repo.continuum.io/miniconda/Miniconda-latest-MacOSX-x86_64.sh -O miniconda.sh
-
-bash miniconda.sh -b -p $HOME/miniconda
-export PATH="$HOME/miniconda/bin:$PATH"
