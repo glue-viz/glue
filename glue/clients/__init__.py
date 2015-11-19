034e1355915c87aa94a561ab149d777ed4d2e7b1
Thomas Robitaille <thomas.robitaille@gmail.com>
2015-11-19 10:55:43 +0000

    Added workaround for Matplotlib to make sure that the backend change gets taken into account

--- glue/clients/__init__.py
+++ glue/clients/__init__.py
@@ -9,6 +9,13 @@ if is_pyqt5():
 else:
     rcParams['backend'] = 'Qt4Agg'
 
+# The following is a workaround for the fact that Matplotlib checks the
+# rcParams at import time, not at run-time. I have opened an issue with
+# Matplotlib here: https://github.com/matplotlib/matplotlib/issues/5513
+from matplotlib import get_backend
+from matplotlib import backends
+backends.backend = get_backend()
+
 from .histogram_client import HistogramClient
 from .image_client import ImageClient
 from .scatter_client import ScatterClient
