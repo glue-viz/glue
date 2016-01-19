b2bc500c6a715493c4187e5b1c62e02717d8e31c
Thomas Robitaille <thomas.robitaille@gmail.com>
2016-01-19 14:10:08 +0000

    Add missing import of dendrogram data factory

--- glue/plugins/dendro_viewer/__init__.py
+++ glue/plugins/dendro_viewer/__init__.py
@@ -1,4 +1,5 @@
 def setup():
-    from .qt_widget import DendroWidget
     from glue.config import qt_client
+    from .qt_widget import DendroWidget
+    from .data_factory import load_dendro
     qt_client.add(DendroWidget)
