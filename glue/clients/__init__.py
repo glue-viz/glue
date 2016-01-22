45187a76a759459ab981c997298d5fdaaa9a9196
Thomas Robitaille <thomas.robitaille@gmail.com>
2016-01-22 22:39:37 +0000

    Add an import hook to always set the Qt backend just before Matplotlib is about to be imported

--- glue/clients/__init__.py
+++ glue/clients/__init__.py
@@ -1,23 +1,5 @@
 from __future__ import absolute_import, division, print_function
 
-from matplotlib import rcParams, rcdefaults
-
-# standardize mpl setup
-rcdefaults()
-
-from glue.external.qt import is_pyqt5
-if is_pyqt5():
-    rcParams['backend'] = 'Qt5Agg'
-else:
-    rcParams['backend'] = 'Qt4Agg'
-
-# The following is a workaround for the fact that Matplotlib checks the
-# rcParams at import time, not at run-time. I have opened an issue with
-# Matplotlib here: https://github.com/matplotlib/matplotlib/issues/5513
-from matplotlib import get_backend
-from matplotlib import backends
-backends.backend = get_backend()
-
 from .histogram_client import HistogramClient
 from .image_client import ImageClient
 from .scatter_client import ScatterClient
