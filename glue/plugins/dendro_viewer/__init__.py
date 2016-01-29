bf630b12acd072688ecbfc0bb801597e19cbc4cc
Thomas Robitaille <thomas.robitaille@gmail.com>
2016-01-29 11:36:26 +0000

    Move Qt code for dendrogram viewer into qt sub-directory

--- glue/plugins/dendro_viewer/__init__.py
+++ glue/plugins/dendro_viewer/__init__.py
@@ -1,5 +1,5 @@
 def setup():
     from glue.config import qt_client
-    from .qt_widget import DendroWidget
+    from .qt.viewer_widget import DendroWidget
     from .data_factory import load_dendro
     qt_client.add(DendroWidget)
