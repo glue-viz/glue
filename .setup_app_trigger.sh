98fa5a318ba099089e3919581382f783cdcfdb07
Thomas Robitaille <thomas.robitaille@gmail.com>
2016-03-03 15:30:00 +0000

    Travis: Avoid issues with gem completion tool

--- .setup_app_trigger.sh
+++ .setup_app_trigger.sh
@@ -1,7 +1,11 @@
 #!/bin/bash -x
 
-gem install travis -v 1.6.11
-gem environment
+# We have to include "yes |" since sometimes, gem asks about installing the
+# shell completion tool.
+
+yes | gem install travis -v 1.6.11
+yes | gem environment
+
 export PATH=`gem environment | grep "EXECUTABLE DIRECTORY" | cut -d":" -f2 | cut -c 2-`:$PATH
 
-echo y | travis branches -r glue-viz/Travis-MacGlue --skip-version-check # install shell completion tool
+yes | travis branches -r glue-viz/Travis-MacGlue --skip-version-check
