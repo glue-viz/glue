f695c96c99bb45867d2e7e3150811492c56921fb
Thomas Robitaille <thomas.robitaille@gmail.com>
2016-05-23 11:58:24 +0100

    Skip test for Matplotlib <1.4

--- glue/viewers/image/qt/tests/test_regression.py
+++ glue/viewers/image/qt/tests/test_regression.py
@@ -6,8 +6,10 @@ import numpy as np
 from glue.core import Data
 from glue.viewers.image.qt import ImageWidget
 from glue.core.tests.util import simple_session
+from glue.tests.helpers import requires_matplotlib_ge_14
 
 
+@requires_matplotlib_ge_14
 @pytest.mark.mpl_image_compare(tolerance=1, savefig_kwargs={'dpi': 50})
 def test_resample_on_zoom():
 
