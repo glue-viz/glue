863d1d4d5e2e015564671a11ccab58d1b863c5e5
Thomas Robitaille <thomas.robitaille@gmail.com>
2016-08-01 18:06:48 +0100

    Make sure we don't emit editingFinished for labels

--- glue/utils/qt/widget_properties.py
+++ glue/utils/qt/widget_properties.py
@@ -176,7 +176,8 @@ class TextProperty(WidgetProperty):
 
     def setter(self, widget, value):
         widget.setText(value)
-        widget.editingFinished.emit()
+        if hasattr(widget, 'editingFinished'):
+            widget.editingFinished.emit()
 
 
 class ButtonProperty(WidgetProperty):
