7a48df86039438057b6aec4e984328601fcbe8f2
Thomas Robitaille <thomas.robitaille@gmail.com>
2016-08-28 19:47:38 +0200

    Fixed toolbar tests

--- glue/viewers/common/qt/tests/test_toolbar.py
+++ glue/viewers/common/qt/tests/test_toolbar.py
@@ -5,21 +5,23 @@ from __future__ import absolute_import, division, print_function
 from qtpy import QtWidgets
 
 from glue.viewers.common.qt.mpl_widget import MplWidget
-
+from glue.viewers.common.qt.data_viewer import DataViewer
 from glue.viewers.common.qt.mouse_mode import MouseMode
 from glue.icons.qt import get_icon
+from glue.core.tests.util import simple_session
 
 from ..mpl_toolbar import MatplotlibViewerToolbar
 
 
 class MouseModeTest(MouseMode):
 
+    mode_id = 'TEST'
+    tool_tip = 'just testing'
+    icon = 'glue_square'
+
     def __init__(self, axes, release_callback=None):
         super(MouseModeTest, self).__init__(axes, release_callback=release_callback)
-        self.icon = get_icon('glue_square')
-        self.mode_id = 'TEST'
         self.action_text = 'test text'
-        self.tool_tip = 'just testing'
         self.last_mode = None
 
     def press(self, event):
@@ -28,65 +30,72 @@ class MouseModeTest(MouseMode):
     def move(self, event):
         self.last_mode = 'MOVE'
 
+class ExampleViewer(DataViewer):
 
-class TestToolbar(object):
+    _toolbar_cls = MatplotlibViewerToolbar
 
-    def setup_method(self, method):
-        self.win = QtWidgets.QMainWindow()
-        widget, axes = self._make_plot_widget(self.win)
-        self.win.setCentralWidget(widget)
-        self.win.central_widget = widget
-        self.canvas = widget.canvas
-        self.axes = axes
-        self.tb = MatplotlibViewerToolbar(self.win)
-        self.mode = MouseModeTest(self.axes, release_callback=self.callback)
-        self.tb.add_mode(self.mode)
-        self.win.addToolBar(self.tb)
-        self._called_back = False
+    def __init__(self, session, parent=None):
+        super(ExampleViewer, self).__init__(session, parent=parent)
+        self.central_widget = MplWidget(parent)
+        self._axes = self.central_widget.canvas.fig.add_subplot(111)
+        self._axes.plot([1, 2, 3])[0]
+        self.setCentralWidget(self.central_widget)
+        self._make_toolbar()
 
-    def teardown_method(self, method):
-        self.win.close()
-
-    def _make_plot_widget(self, parent):
-        widget = MplWidget(parent)
-        ax = widget.canvas.fig.add_subplot(111)
-        p = ax.plot([1, 2, 3])[0]
-        return widget, ax
+    def _make_toolbar(self):
+        super(ExampleViewer, self)._make_toolbar()
+        self.mode = MouseModeTest(self, release_callback=self.callback)
+        self.toolbar.add_mode(self.mode)
 
     def callback(self, mode):
         self._called_back = True
 
+    @property
+    def axes(self):
+        return self._axes
+
+
+class TestToolbar(object):
+
+    def setup_method(self, method):
+        self.session = simple_session()
+        self.viewer = ExampleViewer(self.session)
+        self._called_back = False
+
+    def teardown_method(self, method):
+        self.viewer.close()
+
     def assert_valid_mode_state(self, target_mode):
-        for mode in self.tb.buttons:
-            if mode == target_mode and self.tb.buttons[mode].isCheckable():
-                assert self.tb.buttons[mode].isChecked()
-                self.tb._active == target_mode
+        for mode in self.viewer.toolbar.buttons:
+            if mode == target_mode and self.viewer.toolbar.buttons[mode].isCheckable():
+                assert self.viewer.toolbar.buttons[mode].isChecked()
+                self.viewer.toolbar._active == target_mode
             else:
-                assert not self.tb.buttons[mode].isChecked()
+                assert not self.viewer.toolbar.buttons[mode].isChecked()
 
     def test_callback(self):
-        self.tb.buttons['HOME'].trigger()
-        self.mode.release(None)
-        assert self._called_back
+        self.viewer.toolbar.buttons['HOME'].trigger()
+        self.viewer.mode.release(None)
+        assert self.viewer._called_back
 
     def test_change_mode(self):
 
-        self.tb.buttons['PAN'].toggle()
-        assert self.tb.mode.mode_id == 'PAN'
-        assert self.tb._mpl_nav.mode == 'pan/zoom'
+        self.viewer.toolbar.buttons['PAN'].toggle()
+        assert self.viewer.toolbar.mode.mode_id == 'PAN'
+        assert self.viewer.toolbar._mpl_nav.mode == 'pan/zoom'
 
-        self.tb.buttons['PAN'].toggle()
-        assert self.tb.mode is None
-        assert self.tb._mpl_nav.mode == ''
+        self.viewer.toolbar.buttons['PAN'].toggle()
+        assert self.viewer.toolbar.mode is None
+        assert self.viewer.toolbar._mpl_nav.mode == ''
 
-        self.tb.buttons['ZOOM'].trigger()
-        assert self.tb.mode.mode_id == 'ZOOM'
-        assert self.tb._mpl_nav.mode == 'zoom rect'
+        self.viewer.toolbar.buttons['ZOOM'].trigger()
+        assert self.viewer.toolbar.mode.mode_id == 'ZOOM'
+        assert self.viewer.toolbar._mpl_nav.mode == 'zoom rect'
 
-        self.tb.buttons['BACK'].trigger()
-        assert self.tb.mode is None
-        assert self.tb._mpl_nav.mode == ''
+        self.viewer.toolbar.buttons['BACK'].trigger()
+        assert self.viewer.toolbar.mode is None
+        assert self.viewer.toolbar._mpl_nav.mode == ''
 
-        self.tb.buttons['TEST'].trigger()
-        assert self.tb.mode.mode_id == 'TEST'
-        assert self.tb._mpl_nav.mode == ''
+        self.viewer.toolbar.buttons['TEST'].trigger()
+        assert self.viewer.toolbar.mode.mode_id == 'TEST'
+        assert self.viewer.toolbar._mpl_nav.mode == ''
