3b1e424578002f590124bce44ea659cd66a8c89b
Thomas Robitaille <thomas.robitaille@gmail.com>
2016-09-19 17:08:56 +0100

    Use plot when using same color/size for all points

--- glue/viewers/scatter/layer_artist.py
+++ glue/viewers/scatter/layer_artist.py
@@ -32,59 +32,76 @@ class ScatterLayerArtist(MatplotlibLayerArtist):
         self.data_collection = self.viewer_state.data_collection
 
         # Set up an initially empty artist
-        # self.mpl_artist = self.axes.plot([], [], 'o', mec='none')[0]
-        self.mpl_artists = [self.axes.scatter([0], [0])]
+        self.scatter_artist = self.axes.scatter([], [])
+        self.plot_artist = self.axes.plot([], [], 'o', mec='none')[0]
+
+        self.mpl_artists = self.scatter_artist, self.plot_artist
 
     def update(self):
 
         x = self.layer[self.viewer_state.xatt[0]]
         y = self.layer[self.viewer_state.yatt[0]]
 
-        if self.layer_state.color_mode == 'Fixed':
-            c = self.layer_state.color
-            vmin = vmax = cmap = None
-        else:
-            c = self.layer[self.layer_state.cmap_attribute[0]]
-            vmin = self.layer_state.cmap_vmin
-            vmax = self.layer_state.cmap_vmax
-            cmap = self.layer_state.cmap
-
-        if self.layer_state.size_mode == 'Fixed':
-            s = self.layer_state.size
-        else:
-            s = self.layer[self.layer_state.size_attribute[0]]
-            s = ((s - self.layer_state.size_vmin) /
-                 (self.layer_state.size_vmax - self.layer_state.size_vmin)) * 100
-
-        s *= self.layer_state.size_scaling
-
-        # For plot
-        # self.mpl_artist.set_data(x, y)
-        # self.mpl_artist.set_color(self.color)
-        # self.mpl_artist.set_markersize(self.size)
-
         # TODO: is there a better way to do this?
         # self.clear()
 
-        offsets = np.dstack((x, y))
+        if self.layer_state.size_mode == 'Fixed' and self.layer_state.color_mode == 'Fixed':
 
-        self.mpl_artists[0].set_offsets(offsets)
+            # In this case we use Matplotlib's plot function because it has much
+            # better performance than scatter.
 
-        if self.layer_state.size_mode == 'Fixed':
-            s = np.broadcast_to(s, x.shape)
-        self.mpl_artists[0].set_sizes(s)
+            offsets = np.dstack(([], []))
+            self.scatter_artist.set_offsets(offsets)
 
-        if self.layer_state.color_mode == 'Fixed':
-            self.mpl_artists[0].set_facecolors(c)
+            self.plot_artist.set_data(x, y)
+            self.plot_artist.set_color(self.layer_state.color)
+            self.plot_artist.set_markersize(self.layer_state.size * self.layer_state.size_scaling)
+            self.plot_artist.set_alpha(self.layer_state.alpha)
+
         else:
-            self.mpl_artists[0].set_array(c)
-            self.mpl_artists[0].set_cmap(cmap)
-            self.mpl_artists[0].set_clim(vmin, vmax)
 
-        self.mpl_artists[0].set_edgecolor('none')
+            if self.layer_state.color_mode == 'Fixed':
+                c = self.layer_state.color
+                vmin = vmax = cmap = None
+            else:
+                c = self.layer[self.layer_state.cmap_attribute[0]]
+                vmin = self.layer_state.cmap_vmin
+                vmax = self.layer_state.cmap_vmax
+                cmap = self.layer_state.cmap
 
-        self.mpl_artists[0].set_zorder(self.zorder)
-        self.mpl_artists[0].set_alpha(self.layer_state.alpha)
+            if self.layer_state.size_mode == 'Fixed':
+                s = self.layer_state.size
+            else:
+                s = self.layer[self.layer_state.size_attribute[0]]
+                s = ((s - self.layer_state.size_vmin) /
+                     (self.layer_state.size_vmax - self.layer_state.size_vmin)) * 100
+
+            s *= self.layer_state.size_scaling
+
+            self.plot_artist.set_data([], [])
+
+            offsets = np.dstack((x, y))
+
+            self.scatter_artist.set_offsets(offsets)
+
+            if self.layer_state.size_mode == 'Fixed':
+                s = np.broadcast_to(s, x.shape)
+
+            # Note, we need to square here because for scatter, s is actually
+            # proportional to the marker area, not radius.
+            self.scatter_artist.set_sizes(s ** 2)
+
+            if self.layer_state.color_mode == 'Fixed':
+                self.scatter_artist.set_facecolors(c)
+            else:
+                self.scatter_artist.set_array(c)
+                self.scatter_artist.set_cmap(cmap)
+                self.scatter_artist.set_clim(vmin, vmax)
+
+            self.scatter_artist.set_edgecolor('none')
+
+            self.scatter_artist.set_zorder(self.zorder)
+            self.scatter_artist.set_alpha(self.layer_state.alpha)
 
         # Reset the axes stack so that pressing the home button doesn't go back
         # to a previous irrelevant view.
