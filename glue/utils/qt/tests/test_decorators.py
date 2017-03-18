ac76881afe3635d708818db197bb1326553f066d
Thomas Robitaille <thomas.robitaille@gmail.com>
2017-03-18 11:07:33 +0000

    Fix messagebox_on_error for Qt4

---
+++ glue/utils/qt/tests/test_decorators.py
@@ -0,0 +1,13 @@
+from mock import patch
+
+from ..decorators import messagebox_on_error
+
+
+def test_messagebox_on_error():
+
+    @messagebox_on_error('an error occurred')
+    def failing_function():
+        raise ValueError("Dialog failure")
+
+    with patch('qtpy.QtWidgets.QMessageBox.exec_'):
+        failing_function()
