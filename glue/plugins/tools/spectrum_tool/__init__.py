7cc66d76bbf629b092e3121501fdc38e0f128344
Thomas Robitaille <thomas.robitaille@gmail.com>
2017-06-18 15:01:45 -0400

    Spectrum tool is mostly fixed, but at the moment will just use the first layer to extract the spectrum from, and the moments image overriding doesn't work.

--- glue/plugins/tools/spectrum_tool/__init__.py
+++ glue/plugins/tools/spectrum_tool/__init__.py
@@ -1,4 +1,5 @@
 def setup():
-    from glue.viewers.image.qt import ImageWidget
-    from glue.plugins.tools.spectrum_tool.qt import SpectrumExtractorMode
-    ImageWidget.tools.append('spectrum')
+    from glue.viewers.image_new.qt import ImageViewer
+    from glue.plugins.tools.spectrum_tool.qt import SpectrumExtractorMode  # noqa
+    print(ImageViewer.tools)
+    ImageViewer.tools.append('spectrum')
