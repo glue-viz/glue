03b046c12ebefc603ced9cf2d4cafe488dfee4fb
Thomas Robitaille <thomas.robitaille@gmail.com>
2017-07-11 09:59:49 +0100

    Remove debug print statement

--- glue/plugins/tools/spectrum_tool/__init__.py
+++ glue/plugins/tools/spectrum_tool/__init__.py
@@ -1,5 +1,4 @@
 def setup():
     from glue.viewers.image.qt import ImageViewer
     from glue.plugins.tools.spectrum_tool.qt import SpectrumExtractorMode  # noqa
-    print(ImageViewer.tools)
     ImageViewer.tools.append('spectrum')
