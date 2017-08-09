139f97c09a23dccc70ad3862fa574a4e9f000822
Thomas Robitaille <thomas.robitaille@gmail.com>
2017-08-09 09:58:08 +0100

    Don't reduce the font size as much on Linux and Windows

--- glue/utils/qt/app.py
+++ glue/utils/qt/app.py
@@ -1,3 +1,4 @@
+import platform
 from qtpy import QtCore, QtGui, QtWidgets, PYQT5
 
 __all__ = ['get_qapp']
@@ -6,20 +7,35 @@ qapp = None
 
 
 def get_qapp(icon_path=None):
+
     global qapp
+
     qapp = QtWidgets.QApplication.instance()
+
     if qapp is None:
+
         qapp = QtWidgets.QApplication([''])
         qapp.setQuitOnLastWindowClosed(True)
+
         if icon_path is not None:
             qapp.setWindowIcon(QtGui.QIcon(icon_path))
+
+        if platform.system() == 'Darwin':
+            # On Mac, the fonts are generally too large compared to other
+            # applications, so we reduce the default here. In future we should
+            # make this a setting in the system preferences.
+            size_offset = 2
+        else:
+            # On other platforms, we reduce the font size by 1 point to save
+            # space too. Again, this should probably be a global setting.
+            size_offset = 1
+
         font = qapp.font()
-        font.setPointSize(font.pointSize() - 2)
+        font.setPointSize(font.pointSize() - size_offset)
         qapp.setFont(font)
 
-
     # Make sure we use high resolution icons with PyQt5 for HDPI
-    # displays. TODO: check impact on non-HDPI displays.
+    # displays.
     if PYQT5:
         qapp.setAttribute(QtCore.Qt.AA_UseHighDpiPixmaps)
 
