770660a510063dabc9b8c4f647ed90533a5fd736
Thomas Robitaille <thomas.robitaille@gmail.com>
2017-08-09 19:37:47 +0100

    Fix docs build

--- doc/conf.py
+++ doc/conf.py
@@ -345,7 +345,8 @@ nitpick_ignore = [('py:class', 'object'), ('py:class', 'str'),
                   ('py:class', 'glue.external.echo.core.HasCallbackProperties'),
                   ('py:class', 'glue.external.echo.core.CallbackProperty'),
                   ('py:class', 'glue.external.echo.selection.SelectionCallbackProperty'),
-                  ('py:class', 'glue.viewers.image.state.BaseImageLayerState')
+                  ('py:class', 'glue.viewers.image.state.BaseImageLayerState'),
+                  ('py:class', 'glue.viewers.common.qt.data_viewer_with_state.DataViewerWithState')
               ]
 
 # coax Sphinx into treating descriptors as attributes
