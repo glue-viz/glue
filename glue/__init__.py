0ccdca1fe234873d0642910cc8b663b685de5bf8
Jesse Averbukh <javerbukh@stsci.edu>
2017-10-25 14:26:18 -0400

    Keybindings working; events still fire 3 times

--- glue/__init__.py
+++ glue/__init__.py
@@ -18,6 +18,8 @@ sys.meta_path.append(MatplotlibBackendSetter())
 
 import logging
 from logging import NullHandler
+from . import keyboard_shortcuts
+
 
 logging.getLogger('glue').addHandler(NullHandler())
 
