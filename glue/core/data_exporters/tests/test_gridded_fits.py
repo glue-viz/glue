56e5891ab5078d8dc85183089e1c18c87e392e08
Thomas Robitaille <thomas.robitaille@gmail.com>
2017-11-01 16:37:00 +0000

    Fix exporting of subsets to FITS for integer data

--- glue/core/data_exporters/tests/test_gridded_fits.py
+++ glue/core/data_exporters/tests/test_gridded_fits.py
@@ -7,7 +7,7 @@ from astropy.io import fits
 from ..gridded_fits import fits_writer
 
 
-def test_fits_writer(tmpdir):
+def test_fits_writer_data(tmpdir):
 
     filename = tmpdir.join('test').strpath
 
@@ -19,3 +19,22 @@ def test_fits_writer(tmpdir):
     with fits.open(filename) as hdulist:
         np.testing.assert_equal(hdulist['x'].data, data['x'])
         np.testing.assert_equal(hdulist['y'].data, data['y'])
+
+
+def test_fits_writer_subset(tmpdir):
+
+    filename = tmpdir.join('test').strpath
+
+    data = Data(x=np.arange(6).reshape(2, 3),
+                y=(np.arange(6) * 2).reshape(2, 3))
+
+    subset = data.new_subset()
+    subset.subset_state = data.id['x'] > 2
+
+    fits_writer(filename, subset)
+
+    with fits.open(filename) as hdulist:
+        assert np.all(np.isnan(hdulist['x'].data[0]))
+        assert np.all(np.isnan(hdulist['y'].data[0]))
+        np.testing.assert_equal(hdulist['x'].data[1], data['x'][1])
+        np.testing.assert_equal(hdulist['y'].data[1], data['y'][1])
