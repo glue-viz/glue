2ba87f6ed93243928ffd3c1b318897e2a96d3ab0
Thomas Robitaille <thomas.robitaille@gmail.com>
2018-02-07 15:36:58 +0000

    Be smart about setting the histogram limits to be explicitly the min/max of the data values

--- glue/viewers/histogram/python_export.py
+++ glue/viewers/histogram/python_export.py
@@ -1,3 +1,4 @@
+import numpy as np
 from glue.viewers.common.python_export import code, serialize_options
 
 
@@ -9,13 +10,29 @@ def python_export_histogram_layer(layer, *args):
     script = ""
     imports = []
 
+    x = layer.layer[layer._viewer_state.x_att]
+    x_min = np.nanmin(x)
+    x_max = np.nanmax(x)
+
+    hist_x_min = layer._viewer_state.hist_x_min
+    hist_x_max = layer._viewer_state.hist_x_max
+
     script += "# Get main data values\n"
     script += "x = layer_data['{0}']\n\n".format(layer._viewer_state.x_att.label)
 
     script += "# Set up histogram bins\n"
     script += "hist_n_bin = {0}\n".format(layer._viewer_state.hist_n_bin)
-    script += "hist_x_min = {0}\n".format(layer._viewer_state.hist_x_min)
-    script += "hist_x_max = {0}\n".format(layer._viewer_state.hist_x_max)
+
+    if abs((x_min - hist_x_min) / (hist_x_max - hist_x_min)) < 0.001:
+        script += "hist_x_min = np.nanmin(x)\n"
+    else:
+        script += "hist_x_min = {0}\n".format(hist_x_min)
+
+    if abs((x_max - hist_x_max) / (hist_x_max - hist_x_min)) < 0.001:
+        script += "hist_x_max = np.nanmax(x)\n"
+    else:
+        script += "hist_x_max = {0}\n".format(hist_x_max)
+
     options = dict(alpha=layer.state.alpha,
                    color=layer.state.color,
                    zorder=layer.state.zorder)
