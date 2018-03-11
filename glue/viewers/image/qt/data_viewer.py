4cf198aeea155c9e4f5d11a3c73a8b2560f1bdc7
Thomas Robitaille <thomas.robitaille@gmail.com>
2018-03-11 15:08:59 +0000

    Set tick label size correctly in image viewer export script

--- glue/viewers/image/qt/data_viewer.py
+++ glue/viewers/image/qt/data_viewer.py
@@ -1,5 +1,7 @@
 from __future__ import absolute_import, division, print_function
 
+import os
+
 from astropy.wcs import WCS
 
 from qtpy.QtWidgets import QMessageBox
@@ -32,6 +34,13 @@ IDENTITY_WCS.wcs.ctype = ["X", "Y"]
 IDENTITY_WCS.wcs.crval = [0., 0.]
 IDENTITY_WCS.wcs.crpix = [1., 1.]
 IDENTITY_WCS.wcs.cdelt = [1., 1.]
+
+EXTRA_FOOTER = """
+# Set tick label size - for now tick_params (called lower down) doesn't work
+# properly, but these lines won't be needed in future.
+ax.coords[0].set_ticklabel(size={x_ticklabel_size})
+ax.coords[1].set_ticklabel(size={y_ticklabel_size})
+""".strip()
 
 
 class ImageViewer(MatplotlibDataViewer):
@@ -250,3 +259,8 @@ class ImageViewer(MatplotlibDataViewer):
             pass
 
         return imports, script
+
+    def _script_footer(self):
+        state_dict = self.state.as_dict()
+        imports, script = super(ImageViewer, self)._script_footer()
+        return [], EXTRA_FOOTER.format(**state_dict) + os.linesep * 2 + script
