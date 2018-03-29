f7c39dbcd0a7a63225b1523a711c431c428b75a9
Thomas Robitaille <thomas.robitaille@gmail.com>
2018-03-29 11:46:53 +0100

    Improve UI for saving/exporting data

--- glue/app/qt/application.py
+++ glue/app/qt/application.py
@@ -311,7 +311,7 @@ class GlueApplication(Application, QtWidgets.QMainWindow):
         self._data_toolbar.addWidget(self._button_open_data)
 
         self._button_save_data = QtWidgets.QToolButton()
-        self._button_save_data.setText("Save Data")
+        self._button_save_data.setText("Export Data/Subsets")
         self._button_save_data.setIcon(get_icon('glue_filesave'))
         self._button_save_data.setToolButtonStyle(Qt.ToolButtonTextBesideIcon)
         self._button_save_data.clicked.connect(nonpartial(self._choose_save_data))
@@ -345,7 +345,7 @@ class GlueApplication(Application, QtWidgets.QMainWindow):
         self._data_toolbar.addWidget(self._button_open_session)
 
         self._button_save_session = QtWidgets.QToolButton()
-        self._button_save_session.setText("Save Session")
+        self._button_save_session.setText("Export Session")
         self._button_save_session.setIcon(get_icon('glue_filesave'))
         self._button_save_session.setToolButtonStyle(Qt.ToolButtonTextBesideIcon)
         self._button_save_session.clicked.connect(nonpartial(self._choose_save_session))
@@ -662,6 +662,7 @@ class GlueApplication(Application, QtWidgets.QMainWindow):
         menu.addAction(self._actions['session_reset'])
         menu.addAction(self._actions['session_restore'])
         menu.addAction(self._actions['session_save'])
+        menu.addAction(self._actions['export_data'])
         if 'session_export' in self._actions:
             submenu = menu.addMenu("E&xport")
             for a in self._actions['session_export']:
@@ -850,6 +851,11 @@ class GlueApplication(Application, QtWidgets.QMainWindow):
                    tip='Reset session to clean state')
         a.triggered.connect(nonpartial(self._reset_session))
         self._actions['session_reset'] = a
+
+        a = action('Export D&ata/Subsets', self,
+                   tip='Export data to a file')
+        a.triggered.connect(nonpartial(self._choose_save_data))
+        self._actions['export_data'] = a
 
         a = action("Undo", self,
                    tip='Undo last action',
