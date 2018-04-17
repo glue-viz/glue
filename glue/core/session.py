3338643d8a3d3af3641b3c7f71bdaf4fcf9c8b0a
Thomas Robitaille <thomas.robitaille@gmail.com>
2018-04-17 12:37:18 +0100

    Use weakref in Session to avoid circular references

--- glue/core/session.py
+++ glue/core/session.py
@@ -1,5 +1,7 @@
 from __future__ import absolute_import, division, print_function
 
+import weakref
+
 from glue.core.command import CommandStack
 from glue.core.data_collection import DataCollection
 from glue.core.edit_subset_mode import EditSubsetMode
@@ -10,9 +12,7 @@ class Session(object):
     def __init__(self, application=None, data_collection=None,
                  command_stack=None, hub=None):
 
-        # applications can be added after instantiation
         self.application = application
-
         self.data_collection = data_collection or DataCollection()
         self.hub = self.data_collection.hub
         self.command_stack = command_stack or CommandStack()
@@ -20,3 +20,17 @@ class Session(object):
 
         self.edit_subset_mode = EditSubsetMode()
         self.edit_subset_mode.data_collection = self.data_collection
+
+    @property
+    def application(self):
+        if self._application is None:
+            return None
+        else:
+            return self._application()
+
+    @application.setter
+    def application(self, value):
+        if value is None:
+            self._application = None
+        else:
+            self._application = weakref.ref(value)
