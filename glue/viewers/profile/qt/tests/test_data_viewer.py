bc8f7e49977f9eae6882dcd23345e4d39ecf7a2c
Thomas Robitaille <thomas.robitaille@gmail.com>
2018-04-24 15:51:10 +0100

    Fix profile tests

--- glue/viewers/profile/qt/tests/test_data_viewer.py
+++ glue/viewers/profile/qt/tests/test_data_viewer.py
@@ -65,7 +65,7 @@ class TestProfileViewer(object):
         self.viewer.state.function = nanmean
         assert len(self.viewer.layers) == 1
         layer_artist = self.viewer.layers[0]
-        assert_allclose(layer_artist._visible_data[0], [0, 1, 2])
+        assert_allclose(layer_artist._visible_data[0], [0, 2, 4])
         assert_allclose(layer_artist._visible_data[1], [3.5, 11.5, 19.5])
 
     def test_incompatible(self):
