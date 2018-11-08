872e693cea0836e3184f3fb6d2f2eef066267e03
Thomas Robitaille <thomas.robitaille@gmail.com>
2018-11-08 16:13:13 +0000

    Fix test

--- glue/app/qt/tests/test_edit_subset_mode_toolbar.py
+++ glue/app/qt/tests/test_edit_subset_mode_toolbar.py
@@ -20,35 +20,35 @@ def test_edit_subset_mode_toolbar():
 
     dc.append(Data(x=[1, 2, 3]))
 
-    assert combo_labels(subset_combo) == ('None', ['None'])
+    assert combo_labels(subset_combo) == ('None/Create New', ['None/Create New'])
     assert mode_label.text() == '(the next selection will create a subset)'
 
     sg1 = dc.new_subset_group(subset_state=dc[0].id['x'] > 1, label='Subset 1')
 
-    assert combo_labels(subset_combo) == ('Subset 1', ['Subset 1', 'None'])
+    assert combo_labels(subset_combo) == ('Subset 1', ['Subset 1', 'None/Create New'])
     assert mode_label.text() == 'Mode:'
 
     sg2 = dc.new_subset_group(subset_state=dc[0].id['x'] < 1, label='Subset 2')
 
-    assert combo_labels(subset_combo) == ('Subset 2', ['Subset 1', 'Subset 2', 'None'])
+    assert combo_labels(subset_combo) == ('Subset 2', ['Subset 1', 'Subset 2', 'None/Create New'])
     assert mode_label.text() == 'Mode:'
 
     edit.edit_subset = [sg1, sg2]
 
-    assert combo_labels(subset_combo) == ('Multiple subsets', ['Multiple subsets', 'Subset 1', 'Subset 2', 'None'])
+    assert combo_labels(subset_combo) == ('Multiple subsets', ['Multiple subsets', 'Subset 1', 'Subset 2', 'None/Create New'])
     assert mode_label.text() == 'Mode:'
 
     edit.edit_subset = [sg1]
 
-    assert combo_labels(subset_combo) == ('Subset 1', ['Subset 1', 'Subset 2', 'None'])
+    assert combo_labels(subset_combo) == ('Subset 1', ['Subset 1', 'Subset 2', 'None/Create New'])
     assert mode_label.text() == 'Mode:'
 
     edit.edit_subset = []
 
-    assert combo_labels(subset_combo) == ('None', ['Subset 1', 'Subset 2', 'None'])
+    assert combo_labels(subset_combo) == ('None/Create New', ['Subset 1', 'Subset 2', 'None/Create New'])
     assert mode_label.text() == '(the next selection will create a subset)'
 
     edit.edit_subset = [sg2]
 
-    assert combo_labels(subset_combo) == ('Subset 2', ['Subset 1', 'Subset 2', 'None'])
+    assert combo_labels(subset_combo) == ('Subset 2', ['Subset 1', 'Subset 2', 'None/Create New'])
     assert mode_label.text() == 'Mode:'
