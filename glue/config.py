b19d322be4907e85b9833e116a43242066e39dbd
Thomas Robitaille <thomas.robitaille@gmail.com>
2018-11-09 14:23:13 +0000

    Tidied up code related to custom layer artist maker

--- glue/config.py
+++ glue/config.py
@@ -19,7 +19,8 @@ __all__ = ['Registry', 'SettingRegistry', 'ExporterRegistry',
            'DataExporterRegistry', 'data_exporter', 'layer_action',
            'SubsetMaskExporterRegistry', 'SubsetMaskImporterRegistry',
            'StartupActionRegistry', 'startup_action', 'QtFixedLayoutTabRegistry',
-           'qt_fixed_layout_tab', 'KeyboardShortcut', 'keyboard_shortcut']
+           'qt_fixed_layout_tab', 'KeyboardShortcut', 'keyboard_shortcut',
+           'LayerArtistRegistry', 'layer_artist']
 
 
 CFG_DIR = os.path.join(os.path.expanduser('~'), '.glue')
@@ -737,30 +738,48 @@ class KeyboardShortcut(DictRegistry):
             return func
         return adder
 
-class DataLayerPluginRegistry(Registry):
+
+class LayerArtistMakerRegistry(Registry):
     """
-    
+    A registry that allows customization of layer artists based on the data
+    and viewer type.
     """
 
-    def add(self, datatype, viewertype):
-        """
-        Add a new menubar plugin
-        :param label: Short label for the plugin
-        :type label: str
+    item = namedtuple('LayerArtistMaker', 'label function priority')
 
-        :param function: function
-        :type function: function()
+    def add(self, label, function, priority=0):
         """
-        self.members.append((datatype, viewertype))
+        Add a new plugin for providing custom layer artists.
 
-    def __call__(self, label):
+        Plugins take the form of functions that take two arguments - the data or
+        subset being added, and the viewer. The function should either return a
+        `~glue.viewers.common.layer_artist.LayerArtist` sub-class object or
+        `None`.
+
+        Parameter
+        ---------
+        label : str
+            Name for the plugin
+        function : callable
+            The function that returns layer artists
+        priority : int, optional
+            Set this to a higher number if multiple plugins are present
+            and you want your plugin to take precedence over another.
+        """
+        self.members.append(self.item(label, function, priority))
+
+    def __call__(self, label, priority=0):
         def adder(func):
-            self.add(label, func)
+            self.add(label, func, priority=priority)
             return func
         return adder
 
+    def __iter__(self):
+        for member in sorted(self.members, key=lambda x: -x.priority):
+            yield member
 
-data_layer_registry = DataLayerPluginRegistry()
+
+layer_artist_maker = LayerArtistMakerRegistry()
 qt_client = QtClientRegistry()
 qt_fixed_layout_tab = QtFixedLayoutTabRegistry()
 viewer_tool = ViewerToolRegistry()
