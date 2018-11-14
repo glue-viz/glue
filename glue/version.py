95899320944334214cd775a10d297b8509bb2b2b
Thomas Robitaille <thomas.robitaille@gmail.com>
2018-11-14 14:21:16 +0000

    Back to development: v0.15.0.dev0

--- glue/version.py
+++ glue/version.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import, division, print_function
 
 
-__version__ = '0.14.0'
+__version__ = '0.15.0.dev0'
 
 try:
     from glue._githash import __githash__, __dev_value__  # noqa
