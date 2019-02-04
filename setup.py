8ddf2134c9d3137cf24fa7272cb249806ea440ba
Thomas Robitaille <thomas.robitaille@gmail.com>
2019-02-04 15:20:16 +0000

    Explicitly test for setuptools>=30.3

--- setup.py
+++ setup.py
@@ -1,6 +1,16 @@
 #!/usr/bin/env python
 from __future__ import print_function
 
+import sys
+from distutils.version import LooseVersion
+
+try:
+    import setuptools
+    assert LooseVersion(setuptools.__version__) >= LooseVersion('30.3')
+except (ImportError, AssertionError):
+    sys.stderr.write("ERROR: setuptools 30.3 or later is required\n")
+    sys.exit(1)
+
 from setuptools import setup
 from setuptools.config import read_configuration
 
