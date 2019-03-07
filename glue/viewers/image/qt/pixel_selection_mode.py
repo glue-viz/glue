e6a6a105f93936578c091c1a3c287c9beb1a9b6d
Thomas Robitaille <thomas.robitaille@gmail.com>
2019-03-07 12:04:45 +0000

    Modified pixel selection tool so that it creates a derived dataset instead of a subset

--- glue/viewers/image/qt/pixel_selection_mode.py
+++ glue/viewers/image/qt/pixel_selection_mode.py
@@ -2,10 +2,9 @@ from __future__ import absolute_import, division, print_function
 
 from glue.config import viewer_tool
 
-from glue.core.command import ApplySubsetState
-from glue.core.edit_subset_mode import ReplaceMode
+from glue.core.data_derived import IndexedData
 
-from glue.viewers.matplotlib.toolbar_mode import ToolbarModeBase
+from glue.viewers.common.qt.toolbar_mode import ToolbarModeBase
 from glue.viewers.image.pixel_selection_subset_state import PixelSubsetState
 
 __all__ = ['PixelSelectionTool']
@@ -30,10 +29,10 @@ class PixelSelectionTool(ToolbarModeBase):
         self._move_callback = self._select_pixel
         self._press_callback = self._on_press
         self._release_callback = self._on_release
+        self._derived = None
 
     def _on_press(self, mode):
         self._pressed = True
-        self.viewer.session.edit_subset_mode.mode = ReplaceMode
         self._select_pixel(mode)
 
     def _on_release(self, mode):
@@ -55,13 +54,12 @@ class PixelSelectionTool(ToolbarModeBase):
         x = int(round(x))
         y = int(round(y))
 
-        slices = [slice(None)] * self.viewer.state.reference_data.ndim
-        slices[self.viewer.state.x_att.axis] = slice(x, x + 1)
-        slices[self.viewer.state.y_att.axis] = slice(y, y + 1)
+        indices = [None] * self.viewer.state.reference_data.ndim
+        indices[self.viewer.state.x_att.axis] = slice(x, x + 1)
+        indices[self.viewer.state.y_att.axis] = slice(y, y + 1)
 
-        subset_state = PixelSubsetState(self.viewer.state.reference_data, slices)
-
-        cmd = ApplySubsetState(data_collection=self.viewer._data,
-                               subset_state=subset_state,
-                               override_mode=None)
-        self.viewer._session.command_stack.do(cmd)
+        if self._derived is None:
+            self._derived = IndexedData(self.viewer.state.reference_data, indices)
+            self.viewer.session.data_collection.append(self._derived)
+        else:
+            self._derived.indices = indices
