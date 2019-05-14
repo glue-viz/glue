fc510e81311de0dfb679b04cac4062262d6a8147
Thomas Robitaille <thomas.robitaille@gmail.com>
2019-05-14 16:11:35 +0100

    Fix more docs warnings and move docs building code to separate script

---
+++ .test-docs.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -x
+
+cd doc
+make html linkcheck 2> warnings.log
+
+cat warnings.log
+grep -v "numpy.dtype size changed" warnings.log \
+    | grep -v "numpy.ufunc size changed" \
+    | grep -v "return f(\*args, \*\*kwds)" \
+    > warnings_filt.log
+
+# make sure stderr was empty, i.e. no warnings
+cat warnings_filt.log
+test ! -s warnings_filt.log
+
+  # Check for any broken links, ignore 'redirected with Found'
+grep -v "redirected with Found" _build/linkcheck/output.txt > _build/linkcheck/output_no_found_redirect.txt
+
+# Make sure file is empty, i.e. no warnings/errors
+cat _build/linkcheck/output_no_found_redirect.txt
+test ! -s _build/linkcheck/output_no_found_redirect.txt
