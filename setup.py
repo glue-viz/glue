4193eb2ce0c61f75af0f6ec55f26e60f70153d58
Thomas Robitaille <thomas.robitaille@gmail.com>
2019-06-21 17:48:00 +0100

    Simplify how we deal with PyQt5/PySide2 dependencies - use an extras_require

--- setup.py
+++ setup.py
@@ -12,21 +12,5 @@ except (ImportError, AssertionError):
     sys.exit(1)
 
 from setuptools import setup
-from setuptools.config import read_configuration
 
-# Glue can work with PyQt5 and PySide2. We first check if they are already
-# installed before adding PyQt5 to install_requires, since the conda
-# installation of PyQt5 is not recognized by install_requires.
-
-conf = read_configuration('setup.cfg')
-install_requires = conf['options']['install_requires']
-
-try:
-    import PyQt5  # noqa
-except ImportError:
-    try:
-        import PySide2  # noqa
-    except ImportError:
-        install_requires.append('PyQt5')
-
-setup(use_scm_version=True, install_requires=install_requires)
+setup(use_scm_version=True)
