96618cb2c4354069a18288b202c6e3e6b2017915
Thomas Robitaille <thomas.robitaille@gmail.com>
2019-08-05 16:06:48 +0100

    Started implementing linking for PixelSubsetState

--- glue/viewers/image/pixel_selection_subset_state.py
+++ glue/viewers/image/pixel_selection_subset_state.py
@@ -1,10 +1,52 @@
 from __future__ import absolute_import, division, print_function
 
+import numpy as np
+
 from glue.core.subset import SliceSubsetState
+from glue.core.exceptions import IncompatibleAttribute
+from glue.core.link_manager import pixel_cid_to_pixel_cid_matrix
 
 __all__ = ['PixelSubsetState']
 
 
 class PixelSubsetState(SliceSubsetState):
+
     def copy(self):
         return PixelSubsetState(self.reference_data, self.slices)
+
+    def to_array(self, data, att):
+
+        try:
+
+            return super(PixelSubsetState, self).to_array(data, att)
+
+        except IncompatibleAttribute:
+
+            if data is not self.reference_data:
+
+                # Determine which pixel dimensions are being sliced over
+                dimensions = [idim for idim, slc in enumerate(self.slices) if slc.start is not None]
+
+                # Determine pixel to pixel correlation matrix
+                matrix = pixel_cid_to_pixel_cid_matrix(self.reference_data, data)
+
+                # Find pixel dimensions in 'data' that are correlated
+                correlated_dims = np.nonzero(np.any(matrix[dimensions], axis=0))[0]
+
+                # Check that if we do the operation backwards we just get the
+                # original dimensions back
+                check_dimensions = np.nonzero(np.any(matrix[:, correlated_dims], axis=1))[0]
+
+                if np.array_equal(dimensions, check_dimensions):
+                    pix_coord_in = [slice(0, 1) if slc.start is None else slc for slc in self.slices]
+                    pix_coord_out = []
+                    for idim, pix_cid in enumerate(data.pixel_component_ids):
+                        if idim in correlated_dims:
+                            coord = int(np.round(self.reference_data[pix_cid, pix_coord_in].ravel()[0]))
+                            pix_coord_out.append(slice(coord, coord + 1))
+                        else:
+                            pix_coord_out.append(slice(None))
+
+                    return data[att, tuple(pix_coord_out)]
+
+        raise IncompatibleAttribute()
---
+++ glue/viewers/image/tests/test_pixel_selection_subset_state.py
@@ -0,0 +1,88 @@
+# pylint: disable=I0011,W0613,W0201,W0212,E1101,E1103
+
+from __future__ import absolute_import, division, print_function
+
+import pytest
+
+import numpy as np
+from numpy.testing import assert_array_equal
+
+from glue.core.data import Data
+from glue.core.data_collection import DataCollection
+from glue.core.link_helpers import LinkSame, MultiLink
+from glue.core.exceptions import IncompatibleAttribute
+
+from ..pixel_selection_subset_state import PixelSubsetState
+
+
+def test_pixel_selection_subset_state():
+
+    data1 = Data(x=np.ones((2, 4, 3)))
+    data2 = Data(y=np.ones((4, 3, 2)))
+    data3 = Data(z=np.ones((2, 3)))
+
+    y_id = data2.main_components[0]
+    z_id = data3.main_components[0]
+
+    slice_1d = [slice(1, 2), slice(None), slice(None)]
+    slice_2d = [slice(None), slice(2, 3), slice(1, 2)]
+    slice_3d = [slice(1, 2), slice(2, 3), slice(1, 2)]
+
+    state_1d = PixelSubsetState(data1, slice_1d)
+    state_2d = PixelSubsetState(data1, slice_2d)
+    state_3d = PixelSubsetState(data1, slice_3d)
+
+    states = [state_1d, state_2d, state_3d]
+
+    dc = DataCollection([data1, data2, data3])
+
+    # Calling to_array with reference data should work by default, and not work
+    # with unlinked datasets.
+
+    for data in dc:
+        for state in states:
+            cid = data.main_components[0]
+            if data is data1:
+                assert_array_equal(state.to_array(data, cid), data[cid][state.slices])
+            else:
+                with pytest.raises(IncompatibleAttribute):
+                    state.to_array(data, cid)
+
+    # Add one set of links
+
+    dc.add_link(LinkSame(data1.pixel_component_ids[0], data2.pixel_component_ids[2]))
+    dc.add_link(LinkSame(data1.pixel_component_ids[0], data3.pixel_component_ids[0]))
+
+    assert_array_equal(state_1d.to_array(data2, y_id), data2[y_id][:, :, 1:2])
+    with pytest.raises(IncompatibleAttribute):
+        state_2d.to_array(data2, y_id)
+    with pytest.raises(IncompatibleAttribute):
+        state_3d.to_array(data2, y_id)
+
+    assert_array_equal(state_1d.to_array(data3, z_id), data3[z_id][1:2])
+    with pytest.raises(IncompatibleAttribute):
+        state_2d.to_array(data3, z_id)
+    with pytest.raises(IncompatibleAttribute):
+        state_3d.to_array(data3, z_id)
+
+    # Add links with multiple components, in this case linking two cids with two cids
+
+    def forwards(x, y):
+        return x + y, x - y
+
+    def backwards(x, y):
+        return 0.5 * (x + y), 0.5 * (x - y)
+
+    dc.add_link(MultiLink(data1.pixel_component_ids[1:],
+                          data2.pixel_component_ids[:2],
+                          forwards=forwards, backwards=backwards))
+
+    assert_array_equal(state_1d.to_array(data2, y_id), data2[y_id][:, :, 1:2])
+    assert_array_equal(state_2d.to_array(data2, y_id), data2[y_id][2:3, 1:2, :])
+    assert_array_equal(state_3d.to_array(data2, y_id), data2[y_id][2:3, 1:2, 1:2])
+
+    assert_array_equal(state_1d.to_array(data3, z_id), data3[z_id][1:2])
+    with pytest.raises(IncompatibleAttribute):
+        state_2d.to_array(data3, z_id)
+    with pytest.raises(IncompatibleAttribute):
+        state_3d.to_array(data3, z_id)
