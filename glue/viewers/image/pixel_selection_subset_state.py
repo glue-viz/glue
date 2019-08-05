b558c5eefaae6b6c47b3082367ab2cb6a0bc5eb7
Thomas Robitaille <thomas.robitaille@gmail.com>
2019-08-05 17:13:51 +0100

    Update plotting of crosshairs

--- glue/viewers/image/pixel_selection_subset_state.py
+++ glue/viewers/image/pixel_selection_subset_state.py
@@ -23,30 +23,44 @@ class PixelSubsetState(SliceSubsetState):
         except IncompatibleAttribute:
 
             if data is not self.reference_data:
-
-                # Determine which pixel dimensions are being sliced over
-                dimensions = [idim for idim, slc in enumerate(self.slices) if slc.start is not None]
-
-                # Determine pixel to pixel correlation matrix
-                matrix = pixel_cid_to_pixel_cid_matrix(self.reference_data, data)
-
-                # Find pixel dimensions in 'data' that are correlated
-                correlated_dims = np.nonzero(np.any(matrix[dimensions], axis=0))[0]
-
-                # Check that if we do the operation backwards we just get the
-                # original dimensions back
-                check_dimensions = np.nonzero(np.any(matrix[:, correlated_dims], axis=1))[0]
-
-                if np.array_equal(dimensions, check_dimensions):
-                    pix_coord_in = [slice(0, 1) if slc.start is None else slc for slc in self.slices]
-                    pix_coord_out = []
-                    for idim, pix_cid in enumerate(data.pixel_component_ids):
-                        if idim in correlated_dims:
-                            coord = int(np.round(self.reference_data[pix_cid, pix_coord_in].ravel()[0]))
-                            pix_coord_out.append(slice(coord, coord + 1))
-                        else:
-                            pix_coord_out.append(slice(None))
-
-                    return data[att, tuple(pix_coord_out)]
+                pix_coord_out = self._to_linked_pixel_coords(data)
+                pix_coord_out = tuple([slice(None) if p is None else slice(p, p+1) for p in pix_coord_out])
+                return data[att, pix_coord_out]
 
         raise IncompatibleAttribute()
+
+    def _to_linked_pixel_coords(self, data):
+
+        # Determine which pixel dimensions are being sliced over
+        dimensions = [idim for idim, slc in enumerate(self.slices) if slc.start is not None]
+
+        # Determine pixel to pixel correlation matrix
+        matrix = pixel_cid_to_pixel_cid_matrix(self.reference_data, data)
+
+        # Find pixel dimensions in 'data' that are correlated
+        correlated_dims = np.nonzero(np.any(matrix[dimensions], axis=0))[0]
+
+        # Check that if we do the operation backwards we just get the
+        # original dimensions back
+        check_dimensions = np.nonzero(np.any(matrix[:, correlated_dims], axis=1))[0]
+
+        if np.array_equal(dimensions, check_dimensions):
+            pix_coord_in = tuple([slice(0, 1) if slc.start is None else slc for slc in self.slices])
+            pix_coord_out = []
+            for idim, pix_cid in enumerate(data.pixel_component_ids):
+                if idim in correlated_dims:
+                    coord = int(np.round(self.reference_data[pix_cid, pix_coord_in].ravel()[0]))
+                else:
+                    coord = None
+                pix_coord_out.append(coord)
+
+            return pix_coord_out
+
+        raise IncompatibleAttribute()
+
+    def get_xy(self, data, dim1, dim2):
+        pix_coord_out = self._to_linked_pixel_coords(data)
+        if pix_coord_out[dim1] is None or pix_coord_out[dim2] is None:
+            raise IncompatibleAttribute
+        else:
+            return pix_coord_out[dim1], pix_coord_out[dim2]
