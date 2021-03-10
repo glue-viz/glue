5c31d77220cbba63eff9b7f6b443b18adccca35a
tommy-mor <thmorriss@gmail.com>
2021-03-10 15:39:56 -0800

    fixed syntax error

--- glue/dialogs/component_arithmetic/qt/component_arithmetic.py
+++ glue/dialogs/component_arithmetic/qt/component_arithmetic.py
@@ -215,7 +215,7 @@ class ArithmeticEditorWidget(QtWidgets.QDialog):
         for cid in (self.data.main_components +
                     self.data.pixel_component_ids +
                     self.data.world_component_ids +
-                    [x for x in self._components_derived[self.data] if x is not derived_cid]:
+                    [x for x in self._components_derived[self.data] if x is not derived_cid]):
             if cid is not derived_cid:
                 mapping[cid] = cid.label
                 references[cid.label] = cid
