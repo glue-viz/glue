de678ff5aa418921ac2eacd265ec157dc1a61673
Thomas Robitaille <thomas.robitaille@gmail.com>
2021-06-01 16:44:02 +0100

    Simplify scatter fix and drop Python 3.6 from Azure testing

--- glue/viewers/scatter/layer_artist.py
+++ glue/viewers/scatter/layer_artist.py
@@ -398,12 +398,7 @@ class ScatterLayerArtist(MatplotlibLayerArtist):
                         if self.state.fill:
                             self.scatter_artist.set_edgecolors('none')
                         else:
-                            # The following should work, but doesn't seem to, so instead
-                            # we get the colormap and apply it to the array values ourselves
-                            # colors = self.scatter_artist.get_facecolors()
-                            colors = self.scatter_artist.get_cmap()(self.scatter_artist.get_array())
                             self.scatter_artist.set_facecolors('none')
-                            self.scatter_artist.set_edgecolors(colors)
 
                     if force or any(prop in changed for prop in MARKER_PROPERTIES):
 
