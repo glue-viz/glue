a1ee97b2b2e0c82c8e5ac89ba945c0f6c765a06d
Jonathan Foster <jonathan.bruce.foster@gmail.com>
2021-11-04 16:49:34 -0400

    Get mask in compute_histogram with call that also tries join_on_key

--- glue/core/data.py
+++ glue/core/data.py
@@ -1848,7 +1848,7 @@ class Data(BaseCartesianData):
             w = None
 
         if subset_state is not None:
-            mask = subset_state.to_mask(self)
+            mask = self.get_mask(subset_state)
             if DASK_INSTALLED and isinstance(x, da.Array) and not isinstance(mask, da.Array):
                 x = x[da.asarray(mask)]
             else:
