1a5de0f34644a5c58ee9d97d015be0645642f659
Nabil Freij <nabil.freij@gmail.com>
2022-04-19 21:02:47 -0700

    Update glue/core/tests/test_visual.py

--- glue/core/tests/test_visual.py
+++ glue/core/tests/test_visual.py
@@ -4,17 +4,17 @@ from matplotlib.cm import get_cmap
 
 def test_VA_preferred_cmap():
     # Not a real CMAP array
-    abc = VisualAttributes(preferred_cmap=1)
-    assert abc.preferred_cmap == 1
+    va = VisualAttributes(preferred_cmap=1)
+    assert va.preferred_cmap == 1
 
     # Not a valid string for a CMAP
-    abc = VisualAttributes(preferred_cmap="not_a_cmap")
-    assert abc.preferred_cmap is None
+    va = VisualAttributes(preferred_cmap="not_a_cmap")
+    assert va.preferred_cmap is None
 
     # get_cmap cmap name
-    abc = VisualAttributes(preferred_cmap="viridis")
-    assert abc.preferred_cmap == get_cmap("viridis")
+    va = VisualAttributes(preferred_cmap="viridis")
+    assert va.preferred_cmap == get_cmap("viridis")
 
     # formal cmap name
-    abc = VisualAttributes(preferred_cmap="Viridis")
-    assert abc.preferred_cmap == get_cmap("viridis")
+    va = VisualAttributes(preferred_cmap="Viridis")
+    assert va.preferred_cmap == get_cmap("viridis")
