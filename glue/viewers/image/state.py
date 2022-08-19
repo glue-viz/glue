e0b5d9aa029c67c1b538ecfcd8cb0a60bccec1e0
Thomas Robitaille <thomas.robitaille@gmail.com>
2022-08-19 16:59:20 +0100

    Update glue/viewers/image/state.py
    Co-authored-by: Derek Homeier <dhomeie@gwdg.de>

--- glue/viewers/image/state.py
+++ glue/viewers/image/state.py
@@ -509,6 +509,9 @@ class BaseImageLayerState(MatplotlibLayerState):
         # We now get the fixed resolution buffer
 
         if isinstance(self.layer, BaseData):
+            affine_pretransform = self.viewer_state._affine_pretransform
+            if affine_pretransform is not None:
+                affine_pretransform = affine_pretransform.inverse
             image = self.layer.compute_fixed_resolution_buffer(full_view, target_data=self.viewer_state.reference_data,
                                                                target_cid=self.attribute, broadcast=False, cache_id=self.uuid,
                                                                affine_transform=self.viewer_state._affine_pretransform.inverse)
