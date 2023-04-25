8f12602df783902a8fc84d940cfc89032f97e18a
Jonathan Foster <jonathan.bruce.foster@gmail.com>
2023-04-25 10:36:42 -0400

    Require column for filtering

--- glue/viewers/table/state.py
+++ glue/viewers/table/state.py
@@ -12,14 +12,14 @@ class TableViewerState(ViewerState):
     A state class that includes all the attributes for a table viewer.
     """
 
-    filter_att = SelectionCallbackProperty(docstring='The component/column to filter/search on')
+    filter_att = SelectionCallbackProperty(docstring='The component/column to filter/search on', default_index=0)
     filter = CallbackProperty(docstring='The text string to filter/search on')
 
     def __init__(self, **kwargs):
 
         super(TableViewerState, self).__init__()
 
-        self.filter_att_helper = ComponentIDComboHelper(self, 'filter_att', none=True, categorical=True, numeric=False)
+        self.filter_att_helper = ComponentIDComboHelper(self, 'filter_att', categorical=True, numeric=False)
 
         self.update_from_dict(kwargs)
 
