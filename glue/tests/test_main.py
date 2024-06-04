6169782722a0e0b7393ffb4b2a54be7eeaa81102
CyclingNinja <bennett.sm89@gmail.com>
2024-06-04 15:38:51 +0100

    Complete tests and revert qt_refactor

--- glue/tests/test_main.py
+++ glue/tests/test_main.py
@@ -1,6 +1,7 @@
 from unittest.mock import patch
 
 from glue.main import load_plugins, list_loaded_plugins, list_available_plugins
+from glue._plugin_helpers import REQUIRED_PLUGINS
 
 
 def test_load_plugins(capsys):
@@ -34,27 +35,38 @@ def test_no_duplicate_loading(capsys):
     with patch.object(logger, 'info') as info:
         load_plugins()
 
+    plugins = []
     for acall in info.call_args_list:
+        plugins.append(acall[0][0])
         if 'Loading plugin' in acall[0][0]:
             assert 'failed' in acall[0][0]
+
+    loaded_plugins = list_loaded_plugins()
+    assert 'glue.plugins.wcs_autolinking' in loaded_plugins
+    assert 'glue.core.data_exporters' in loaded_plugins
+    assert 'glue.plugins.coordinate_helpers' in loaded_plugins
+    assert len(loaded_plugins) == 5
 
 
 def test_list_loaded_plugins():
     """
-    Regression test for retrieving the list of currently loaded plugins
+    Unit test for retrieving the list of currently loaded plugins
     """
     load_plugins(require_qt_plugins=False)
     plugins = list_loaded_plugins()
     assert isinstance(plugins, list)
     assert len(plugins) == 5
+    for test_plugin in REQUIRED_PLUGINS:
+        assert test_plugin in plugins
 
 
 def test_list_available_plugins():
     """
-    Regression test for retrieving the list of currently available plugins
+    Unit test for retrieving the list of currently available plugins
     """
     available_plugins = list_available_plugins()
     assert isinstance(available_plugins, list)
     assert len(available_plugins) == 7
-    assert 'casa_formats_io.glue_factory' in available_plugins
     assert 'glue.plugins.wcs_autolinking' in available_plugins
+    assert 'glue.core.data_exporters' in available_plugins
+    assert 'glue.plugins.coordinate_helpers' in available_plugins
