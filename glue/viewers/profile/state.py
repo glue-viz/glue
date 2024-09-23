c407b70e58e20742c7a24aed9668ea78d38a9bd7
P. L. Lim <2090236+pllim@users.noreply.github.com>
2024-09-23 13:43:56 -0400

    Update glue/viewers/profile/state.py
    [ci skip]
    
    Co-authored-by: Derek Homeier <dhomeie@gwdg.de>

--- glue/viewers/profile/state.py
+++ glue/viewers/profile/state.py
@@ -231,9 +231,8 @@ class ProfileViewerState(MatplotlibDataViewerState):
                     self.y_min = y_min
                     self.y_max = y_max
                 elif np.allclose(y_min, y_max):
-                    dy = max(0.1, 0.1 * y_min)
-                    self.y_min = y_min - dy
-                    self.y_max = y_max + dy
+                    self.y_min = y_min * 0.9
+                    self.y_max = y_max * 1.1
                 else:
                     self.y_min = 0
                     self.y_max = 1
